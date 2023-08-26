9e7e2f326fbb951dfe2492e289d9a14edfda37b5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-26 15:40:04 -0500

    completed javascript for form and list inputs...verified the functions work with the HTML page and have succefully entered and added 20 books I personally own and have read from a large list of books -KS

--- 100-projects-004-book-list1.js
+++ 100-projects-004-book-list1.js
@@ -5,4 +5,9 @@ document.getElementById('book-form').addEventListener("submit", function(event) 
 
     let title = document.getElementById('title').value;
     let author = document.getElementById("author").value;
-}
+
+    let bookItem = document.createElement("li");
+    bookItem.textContent = title + " by " + author;
+
+    document.getElementById('book-list').appendChild(bookItem);
+});
