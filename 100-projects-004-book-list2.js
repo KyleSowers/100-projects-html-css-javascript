63136e75317773d416a681e8fb3dac49ec8be1cf
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-26 16:41:27 -0500

    Added around half javascript needed for functionality -KS

--- 100-projects-004-book-list2.js
+++ 100-projects-004-book-list2.js
@@ -1,1 +1,25 @@
 "use strict"
+
+const bookForm = document.getElementById("book-form");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const bookList = document.getElementById("book-list");
+
+bookForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const title = titleInput.value;
+    const author = authorInput.value;
+
+    addBookToList(title, author);
+    titleInput.value = ";
+    authorInput.value = ";
+    titleInput.focus();
+});
+
+function addBookToList(title, author) {
+    const bookItem =document.createElement("li");
+    bookItem.className = "book-item";
+
+
+}
