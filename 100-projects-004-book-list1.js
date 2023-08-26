25d485e618c1373fcecdb79dcb5b46443f2c4ad8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-26 15:22:59 -0500

    Added JavaScript for HTML form element -Ks

---
+++ 100-projects-004-book-list1.js
@@ -0,0 +1,8 @@
+"use strict"
+
+document.getElementById('book-form').addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let author = document.getElementById("author").value;
+}
