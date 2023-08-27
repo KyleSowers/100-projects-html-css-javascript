e621312e11a32a81a2cb9fcb2951a5f6211e6a72
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-27 10:29:15 -0500

    completed JavaScript button toggle function...confirmed working for user on the HTML page -KS

--- 100-projects-005-resume1.js
+++ 100-projects-005-resume1.js
@@ -1,1 +1,14 @@
 "use strict"
+
+document.getElementById("toggle-button").onclick = function() {
+    let content = document.getElementById("business-content");
+    let header = document.getElementById("business-header");
+
+    if (content.style.display === "none") {
+        content.style.display = "block";
+        header.style.display = "block";
+    } else {
+        content.style.display = "none";
+        header.style.display = "none";
+    }
+}
