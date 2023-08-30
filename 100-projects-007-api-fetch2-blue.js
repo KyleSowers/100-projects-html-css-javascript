a01db9b530c958efae80ecb61da4988af2a3f8df
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-29 20:39:08 -0500

    simple and familiar to project 006  javaScript img retrieval function incoporated -KS

--- 100-projects-007-api-fetch2-blue.js
+++ 100-projects-007-api-fetch2-blue.js
@@ -1,1 +1,8 @@
 "use strict"
+
+for (let i = 0; i < 100; i++) {
+    let img =document.createElement('div');
+    img.className = 'image';
+    img.style.backgroundImage = 'url(https://via.placeholder.com/640x630/' + (200 + i % 10) + '/' + (200 + i % 10) + ')';
+    document.body.appendChild(img);
+}
