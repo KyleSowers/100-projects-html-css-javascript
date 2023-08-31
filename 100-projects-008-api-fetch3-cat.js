7320abf9feb8feea1d83b98fb44e348d952cdd46
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-30 22:42:14 -0500

    image properties width height & padding have applied images from JS to page -KS

--- 100-projects-008-api-fetch3-cat.js
+++ 100-projects-008-api-fetch3-cat.js
@@ -3,6 +3,6 @@
 for (let i = 0; i < 100; i++) {
     let img = document.createElement('div');
     img.className = 'image';
-    img.style.backgroundImage = 'url(https://loremflickr.com/' + (200 + i % 10) + '/' + (200 i % 10) +')';
+    img.style.backgroundImage = 'url(https://loremflickr.com/' + (200 + i % 10) + '/' + (200 + i % 10) +')';
     document.body.appendChild(img);
 }
