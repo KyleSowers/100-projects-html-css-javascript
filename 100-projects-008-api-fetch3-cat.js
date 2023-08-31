435999af7c790a62bcac65566266c13e698df22b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-30 22:38:15 -0500

    javascript as in 100-project-006 & 007 function in place...needed css properties next -KS

---
+++ 100-projects-008-api-fetch3-cat.js
@@ -0,0 +1,8 @@
+"use strict"
+
+for (let i = 0; i < 100; i++) {
+    let img = document.createElement('div');
+    img.className = 'image';
+    img.style.backgroundImage = 'url(https://loremflickr.com/' + (200 + i % 10) + '/' + (200 i % 10) +')';
+    document.body.appendChild(img);
+}
