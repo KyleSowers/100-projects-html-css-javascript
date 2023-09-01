1038553524c77984a8858ab6aad376aaf558d177
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-08-31 22:10:01 -0500

    applied and tested the function element that uses total and desired tip percentge to provide tip result to user -KS

--- 100-projects-009-tip-calculator2.js
+++ 100-projects-009-tip-calculator2.js
@@ -1,1 +1,10 @@
 'use strict'
+
+function calculateTip() {
+    let bill = document.getElementById('bill').value;
+    let percentage = document.getElementById('percentage').value;
+
+    let tip = bill * (percentage / 100);
+
+    document.getElementById('result').innerHTML = 'The tip amount is: $' + tip.toFixed(2);
+}
