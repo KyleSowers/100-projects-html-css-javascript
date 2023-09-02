9392c3e1557464923cd75da48b06b1f2be24f0f2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-01 21:55:49 -0500

    Although css properties were not the focus of the first registration form project for today, I added my own css properties for viewer presentaation -KS

--- 100-projects-010-registration-form1.js
+++ 100-projects-010-registration-form1.js
@@ -1,1 +1,33 @@
 "use strict"
+
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let feedback = document.getElementById("feedback");
+
+    if (name == "") {
+        feedback.innerHTML = "Name is required.";
+        return;
+    }
+
+    if (email == "") {
+        feedback.innerHTML = "Email is required.";
+        return;
+    }
+
+    if (password == "") {
+        feedback.innerHTML = "Password is required.";
+        return;
+    }
+
+    if (password.length < 8) {
+        feedback.innerHTML = "Password must be at least 8 characters.";
+        return;
+    }
+
+    feedback.innerHTML = "Registration successful!";
+});
