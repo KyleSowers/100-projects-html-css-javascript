5d2e6d6ddb75d50699b9c09783ab43ee2ce5bd4c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-05 19:06:18 -0500

    Javascript added to evaluate quiz and present result..texted succesfully -KS

--- 100-projects-014-sample-quiz1.js
+++ 100-projects-014-sample-quiz1.js
@@ -1,1 +1,18 @@
 'use strict'
+
+document.getElementById('quiz-form').addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let score = 0;
+
+    if (document.getElementById('q1').value.trim().toLowerCase()==='paris') {
+        score++;
+    }
+    if (document.getElementById('q2').value.trim().toLowerCase()==='george orwell') {
+        score++;
+    }
+    if (document.getElementById('q3').value.trim().toLowerCase()==='9') {
+        score++
+    }
+    document.getElementById('score').textContent='You scored ' + score + ' out of 3.';
+});
