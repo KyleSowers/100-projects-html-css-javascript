2ffe02b8b459adce90f999ef90c88ef3af361c35
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-06 20:42:48 -0500

    Added JavaScript functionality -KS

--- 100-projects-015-temperature-converter2.js
+++ 100-projects-015-temperature-converter2.js
@@ -1,1 +1,14 @@
 'use strict'
+
+function convertTemperature() {
+    let temp = document.getElementById('temp').value;
+    let output = document.getElementById('output');
+
+    if (document.getElementById('f_to_c').checked) {
+        output.innerHTML = ((temp -32) * 5 / 9).toFixed(2);
+    } else if (document.getElementById('c_to_f').checked) {
+        output.innerHTML = ((temp * 9 / 5) + 32).toFixed(2);
+    } else {
+        output.innerHTML = "Please select conversion method.";
+    }
+}
