1f9797602304733378d15d8434f94305fed79158
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-07 20:34:21 -0500

    added to javascript functions to allow onclick to provide random numbers -KS

--- 100-projects-016-dice-lotto-roll1.js
+++ 100-projects-016-dice-lotto-roll1.js
@@ -1,1 +1,11 @@
 'use strict'
+
+document.getElementById('roll-dice-button').onclick = function() {
+    let result = Math.floor(Math.random() * 6) +1;
+    document.getElementById('result').innerHTML = result;
+};
+
+document.getElementById('roll-dice-button2').onclick = function() {
+    let result = Math.floor(Math.random() * 45) + 1;
+    document.getElementById('result2').innerHTML = result;
+};
