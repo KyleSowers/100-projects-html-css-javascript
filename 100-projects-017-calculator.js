c8892c59c12bfaddc010f43a9074106c1e3b2122
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-08 22:09:32 -0500

    Added js to map inputs and provide result in calculator -KS

--- 100-projects-017-calculator.js
+++ 100-projects-017-calculator.js
@@ -1,1 +1,31 @@
 'use strict'
+
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+let operators = Array.from(document.getElementsByClassName('operator'));
+let equals = document.getElementById('equals');
+let clear = document.getElementById('clear');
+
+buttons.map(button =>{
+    button.addEventListener('click', (e) => {
+        display.value += e.target.value;
+    })
+});
+
+operators.map(operator => {
+    operator.addEventListener('click', (e) => {
+        display.value += e.target.value;
+    })
+});
+
+equals.addEventListener('click', () => {
+    try{
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+});
+
+clear.addEventListener('click', () => {
+    display.value = "";
+});
