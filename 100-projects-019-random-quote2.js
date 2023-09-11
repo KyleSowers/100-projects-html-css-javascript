ce1d08960cac5c3e457cf7f4dd87a30787a7c6bc
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-10 20:47:43 -0500

    created html/css/js files for 100-projects-019-random-quote2 project..added js array of quotes from 100-projects-019-random-quotes1 - KS

---
+++ 100-projects-019-random-quote2.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//This array will hold the quotes that will be randomly displayed
+let quotes = [
+    "Somewhere, something increadible is waiting to be known.",
+    "We are a way for the cosmos to know itself.",
+    "Science is a way of thinking much more than it is a body of knowledge.",
+    "For small creatures such as we the vastness is bearable only through love.",
+    "We're made of star stuff. We are a way for the universe to know itself.",
+    "Extraordinary claims require extraordinary evidence.",
+    "In science it often happens that scientists say, 'You know that's a really good arguement; my position is mistaken,' and then they would actually change their minds and you never hear that old view from them again.",
+    "The cosmos is within us. We are made of star-stuff. We are a way for the universe to know itself.",
+    "The beauty of a living thing is not the atoms that go into it, but the way those atoms are put together.",
+    "It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring.",
+    "We can judge our progress by the courage of our questions and the depth of our answers, our willingness to embrace what is true rather what feels good.",
+    "The universe is not required to be in perfect harmony with human ambition.",
+    "One of the saddest lessons of history is this: If we've been bamboozled long enough, we tend to reject any evidence of the bamboozle. We're no longer interested in finding out the truth.",
+    "Look again at that dot. That's here. That's home. That's us. On it everyone you love, everyone you know, everyone you ever heard of, every human being who ever was, lived out their lives.",
+    "Imagination will often carry us to worlds that never were, but without it we go nowhere.",
+    "The nitrogen in our DNA, the calcium in out teeth, the iron in our blood, the carbon in our apple pies were made in the interiors of collapsing stars. We are made of star-stuff.",
+    "Who are we? We find that we live on an insignificant planet of a humdrum star lost in a galaxy tucked away in some forgotten corner of a universe in which their are far more galaxies than people.",
+    "Books break the shackles of time - proof that humans can work magic.",
+    "It pays to keep an open mind, but not so open your brains fall out.",
+    "Our species needs, and deserves, a citizendry with minds wide awake and a basic understanding of how the world works."
+];
