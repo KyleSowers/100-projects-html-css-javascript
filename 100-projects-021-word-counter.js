8af53ab5949bfe8ea360c31b93b7b91cc0afd82e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-12 19:51:31 -0500

    added js functioning that counts textarea..then provides a character count..then word count..then uses the added event listener to run function upon user request -KS

--- 100-projects-021-word-counter.js
+++ 100-projects-021-word-counter.js
@@ -1,1 +1,19 @@
 'use strict'
+
+//This function counts the number of words and characters in the user's text
+function countWordsAndCharacters() {
+    let text = document.getElementById('user-input').value;
+
+//Count characters
+    let characterCount = text.length;
+    document.getElementById('character-count').textContent = 'Character count:' + characterCount;
+
+//Count words
+    let wordCount = text.split(/\s+/).filter(function (word) {
+        return word.length > 0;
+    }).length;
+    document.getElementById('word-count').textContent = 'Word count: ' + wordCount;
+}
+
+//This event listener calls the countWordsAndCharacters function each time the button is clicked
+document.getElementById('count-button').addEventListener('click', countWordsAndCharacters);
