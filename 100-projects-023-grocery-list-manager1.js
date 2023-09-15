d7a97742cd2fa02ddfe6d107efd8ada25c946f79
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-14 21:35:20 -0500

    created event listener function to call from the input, log input into variable, input text of variable to become a list item, then added a delete button and a bought buttom to toggle (strike line thought) with click event listeners, then finally appended the new buttons and li items into the ul in the HTML. Actions verify in the HTML...project complete! - KS

--- 100-projects-023-grocery-list-manager1.js
+++ 100-projects-023-grocery-list-manager1.js
@@ -1,1 +1,31 @@
 'use strict'
+
+document.getElementById('add-button').addEventListener('click', function() {
+    let itemInput = document.getElementById('item-input');
+    let itemText = itemInput.value;
+    itemInput.value = '';
+
+    //This will creat li's and display text
+    let listItem = document.createElement('li');
+    listItem.textContent = itemText;
+
+    //Creating delete button that will display and function in HTML
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('item-button');
+    deleteButton.addEventListener('click', function () {
+        listItem.remove();
+    });
+
+    //Creating a bought button to place a line-through a bought item
+    let boughtButton =document.createElement('button');
+    boughtButton.textContent = "Bought";
+    boughtButton.classList.add('item-button');
+    boughtButton.addEventListener('click', function () {
+        listItem.classList.toggle('bought');
+    });
+
+    listItem.appendChild(deleteButton);
+    listItem.appendChild(boughtButton);
+    document.getElementById('grocery-list').appendChild(listItem);
+})
