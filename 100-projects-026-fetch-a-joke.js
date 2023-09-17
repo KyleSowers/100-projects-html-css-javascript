79edf6b0f46e26354c2c714f42309f04e3053dcc
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-17 09:16:27 -0500

    Added js function to call upon click event, then fetch a random joke with punchline from a url for the project. It then passes the text.content through to the joke container - KS

--- 100-projects-026-fetch-a-joke.js
+++ 100-projects-026-fetch-a-joke.js
@@ -1,1 +1,10 @@
 'use strict'
+document.getElementById('fetch-button').addEventListener('click', function () {
+    fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => response.json())
+        .then(data => {
+            var jokeContainer =document.getElementById('joke-container');
+            jokeContainer.textContent = data.setup + ' ' + data.punchline;
+        })
+        .catch(error => console.error('Error:', error));
+})
