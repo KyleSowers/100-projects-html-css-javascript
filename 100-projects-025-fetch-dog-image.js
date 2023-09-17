cf5f5207726f69110d5aee302879ca65a2554813
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-16 21:15:50 -0500

    JS functioning using button listener event is calling an api to provide random dog photos on screen -KS

--- 100-projects-025-fetch-dog-image.js
+++ 100-projects-025-fetch-dog-image.js
@@ -1,1 +1,14 @@
 'use strict'
+
+document.getElementById('fetch-button').addEventListener('click', function() {
+    fetch('https://api.thedogapi.com/v1/images/search')
+    .then(response => response.json())
+        .then(data => {
+            let imageContainer = document.getElementById('image-container');
+            let img =document.createElement('img');
+            img.src = data[0].url;
+            imageContainer.innerHTML = '';
+            imageContainer.appendChild(img);
+        })
+        .catch(error => console.error('Error:', error));
+})
