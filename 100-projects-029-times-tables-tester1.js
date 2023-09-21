fe4f1197105fbd28d0f50a67cf5f9b1c76d46015
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-20 22:22:15 -0500

    Event listener, value calls, multiplication, verification and responce funtioniing added for accurate times tabels tester in html on DOM -KS

--- 100-projects-029-times-tables-tester1.js
+++ 100-projects-029-times-tables-tester1.js
@@ -1,1 +1,29 @@
 'use strict'
+
+document.getElementById('submit').addEventListener('click', checkAnswer);
+
+let num1, num2, correctAnswer;
+
+function generateQuestion() {
+    num1 = Math.floor(Math.random() * 10) + 1;
+    num2 = Math.floor(Math.random() * 10) + 1;
+    correctAnswer = num1 * num2;
+    document.getElementById('question').innerText = `What is ${num1} x ${num2}?`;
+}
+
+function checkAnswer () {
+    let userAnswer = document.getElementById('answer').value;
+    if (userAnswer == correctAnswer) {
+        document.getElementById('feedback').innerText = 'Correct! Great job.';
+    } else {
+        document.getElementById('feedback').innerText = `Sorry, that's incorrect. The correct answer was ${correctAnswer}.`;
+    }
+        //Generate a new question
+        generateQuestion();
+        //Clear the input field
+        document.getElementById('answer').value = '';
+        //set focus back on the input field
+        document.getElementById('answer').focus();
+}
+    //Generate the first queestion as soon as the page loads
+    generateQuestion();
