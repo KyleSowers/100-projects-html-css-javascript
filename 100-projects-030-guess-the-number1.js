61d9605f346aa771001265329c5e7ca06289d73b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-21 21:08:14 -0500

    JS click event, function for idetifying input, discovering match and providing appropriate response to user complete - KS

--- 100-projects-030-guess-the-number1.js
+++ 100-projects-030-guess-the-number1.js
@@ -1,1 +1,32 @@
 'use strict'
+
+var counter = 0;
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+document.getElementById('try').onkeypress = function (e) {
+    if (e.keyCode == 13 || e.which == 13) {
+        finding();
+        retrun false;
+    }
+}
+
+function finding() {
+    var userNumber = document.getElementById('try').value;
+
+    if (userNumber >=1 && userNumber <= 100) {
+        if (randomNumber > userNumber) {
+            document.getElementById('display').innerText = "Too  Low! Try Again!";
+        } else if (randomNumber < userNumber) {
+            document.getElementById('display').innerText = "Too High! Try Again!";
+        } else {
+            document.getElementById('display').innerHTML = "<span style='color:blue'>You are correct!</span>";
+        }
+
+        document.getElementById('try').value = '';
+        counter++;
+        document.querySelector('#counter').innerHTML = "Try Number : " + counter + " time(s)";
+    } else
+        alert("Input a number between 1 and 100.");
+
+    document.getElementById('try').focus();
+}
