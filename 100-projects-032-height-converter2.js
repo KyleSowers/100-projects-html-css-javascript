e4a53864a8196a840e92a406cbd602fd0d7fb1cf
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-23 09:10:48 -0500

    Added calls for event listeners. Additionally from 100-projects-032-height-converter1 project, we have added a reset button that makes an additonal event listener based on click of reset button, which should  add a function into the js -KS" git push origin main

--- 100-projects-032-height-converter2.js
+++ 100-projects-032-height-converter2.js
@@ -1,2 +1,6 @@
 'use strict'
 
+document.getElementById('inputHeight').addEventListener('input', convertHeight);
+document.getElementById('inputUnit').addEventListener('change', convertHeight);
+document.getElementById('outputUnit').addEventListener('change', convertHeight);
+document.getElementById('reset').addEventListener('click', resetconverter);
