a4f74d6d89afd36d25c9a6060bc62c1370bf7923
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-23 09:21:00 -0500

    JS functioning added (event listeners, function w/ equation for conversion) and the newly created reset function to set values back to emtpy string result for user -KS"

--- 100-projects-032-height-converter2.js
+++ 100-projects-032-height-converter2.js
@@ -3,4 +3,45 @@
 document.getElementById('inputHeight').addEventListener('input', convertHeight);
 document.getElementById('inputUnit').addEventListener('change', convertHeight);
 document.getElementById('outputUnit').addEventListener('change', convertHeight);
-document.getElementById('reset').addEventListener('click', resetconverter);
+document.getElementById('reset').addEventListener('click', resetConverter);
+
+function convertHeight() {
+    let inputHeight = document.getElementById('inputHeight').value;
+    let inputUnit = document.getElementById('inputUnit').value;
+    let outputUnit = document.getElementById('outputUnit').value;
+
+    let baseHeightInCm;
+
+    switch (inputUnit) {
+        case 'cm':
+            baseHeightInCm = inputHeight;
+            break;
+        case 'in':
+            baseHeightInCm = inputHeight * 2.54;
+            break;
+        case 'ft':
+            baseHeightInCm = inputHeight * 30.48;
+            break;
+    }
+
+    let outputHeight;
+
+    switch (outputUnit) {
+        case 'cm':
+            outputHeight = baseHeightInCm;
+            break;
+        case 'in':
+            outputHeight = baseHeightInCm / 2.54;
+            break;
+        case 'ft':
+            outputHeight = baseHeightInCm /30.48;
+            break;
+    }
+
+    document.getElementById('outputHeight').value = outputHeight;
+}
+
+function resetConverter() {
+    document.getElementById('inputHeight').value = '';
+    document.getElementById('outputHeight').value = '';
+}
