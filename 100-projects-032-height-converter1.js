f364d79ec6ced84e5e0214abbd19a9e36b686f78
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-23 08:36:17 -0500

    Added input and selector event listeners -KS

--- 100-projects-032-height-converter1.js
+++ 100-projects-032-height-converter1.js
@@ -1,2 +1,5 @@
 'use srict'
 
+document.getElementById('inputHeight').addEventListener('input', convertHeight);
+document.getElementById('inputUnit').addEventListener('change', convertHeight);
+document.getElementById('outputUnit').addEventListener('change', convertHeight);
