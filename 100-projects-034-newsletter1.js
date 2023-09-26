8ffafe6e605d8b82b42221167d2ef2faf17d8a7e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-25 19:31:38 -0500

    Created event listener, stored email value and returns in console log for verification -KS

--- 100-projects-034-newsletter1.js
+++ 100-projects-034-newsletter1.js
@@ -1,2 +1,10 @@
 'use strict'
 
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    //prevent the form from submitting normally
+    event.preventDefault();
+
+    let email = document.getElementById('email').value;
+
+    console.log('Form Submitted! Email: ', email);
+})
