88fe924559515e174bff816958e90f441365f76f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-26 19:46:05 -0500

    Created adjective and noun arrays. Called upon those with a randomizer function. Placed the result into a funcion and called the output to be stored in the <p> html -KS

--- 100-projects-035-user-name-generator1.js
+++ 100-projects-035-user-name-generator1.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const adjectives = ['Awesome', 'Shiny', 'Fast', 'Clever', 'Brave'];
+const nouns = ['Unicorn', 'Turtle', 'Rocket', 'Knight', 'Coder'];
+
+function getRandomElement(array) {
+    const randomIndex =Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+function generateUsername() {
+    const randomAdjective = getRandomElement(adjectives);
+    const randomNoun = getRandomElement(nouns);
+    return randomAdjective + randomNoun;
+}
+
+document.getElementById('generate-button').addEventListener('click', function() {
+    const username = generateUsername();
+    document.getElementById('username-display').textContent = username;
+})
