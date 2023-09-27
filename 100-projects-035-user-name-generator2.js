2569972e8be11ff45f424f8a8ee3191b23102f1a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-26 20:13:25 -0500

    Added slightly more complex adjective & noun arrays to pull from. They are created within the generate function compared to outside on version 1 of this project. Otherwise called and displayed similarly through js -KS

--- 100-projects-035-user-name-generator2.js
+++ 100-projects-035-user-name-generator2.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const generateUsername = () => {
+    const adjectives = ['Adept', 'Brave', 'Cautious', 'Daring', 'Efficient', 'Fearless', 'Gracious', 'Humble', 'Imaginative', 'Joyful', 'Keen', 'Loyal', 'Meticuluos', 'Noble', 'Optimistic', 'Patient', 'Quick', 'Reliable', 'Sincere', 'Trustworthy'];
+    const  nouns = ['Liam', 'noah', 'Oliver', 'James', 'Elijah', 'Henry', 'William', 'Lucas', 'Benjamin', 'Theodore', 'Olivia', 'Emma', 'Amelia', 'Charlotte', "Ava", 'Sophia', 'Luca', 'Waylon',  'Kai', 'HUdson'];
+
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const noun = nouns[Math.floor(Math.random() * nouns.length)];
+
+    return `${adjective}  ${noun}`;
+}
+
+document.getElementById('generate-button').addEventListener('click', () => {
+    document.getElementById('username-display').textContent = generateUsername();
+})
