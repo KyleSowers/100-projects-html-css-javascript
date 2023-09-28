2335272631d48593969511035c0e396fff66f012
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-27 21:48:33 -0500

    JS function to call upon users birthdate input compared to the current date, which provides the output of the users age has been completed -KS"

--- 100-projects-036-age-calculator.js
+++ 100-projects-036-age-calculator.js
@@ -1,1 +1,19 @@
 'use strict'
+
+document.getElementById('age-calculator-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const birthdate = new
+    Date(document.getElementById('birthdate').value);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const month =today.getMonth() - birthdate.getMonth();
+
+    if (month < 0 || (month ===0 && today.getDate() < birthdate.getDate())) {
+        age--;
+    }
+
+    document.getElementById('result').textContent = "You are " + age + " years old.";
+})
+
