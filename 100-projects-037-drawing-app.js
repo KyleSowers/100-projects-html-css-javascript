0d897d0249e94539f9fc7c2513b13b013f7cd929
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-28 20:20:38 -0500

    Added JS functionality to allow 2D drawing on the event of the mouse down/move/up -KS" ;

--- 100-projects-037-drawing-app.js
+++ 100-projects-037-drawing-app.js
@@ -1,2 +1,28 @@
 'use strict'
 
+const canvas = document.getElementById('drawing-area');
+const ctx = canvas.getContext('2d');
+let drawing = false;
+
+canvas.addEventListener('mousedown', e => {
+    drawing = true;
+    draw(e);
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => {
+    drawing = false;
+    ctx.beginPath();
+});
+
+function draw(e) {
+    if (!drawing) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+}
