ef13afae9995b75b8be0352fd659b1b12da1b66c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-29 14:25:32 -0500

    All CSS properties (to include an interactive button style when ready to submit for nmi result) have been added. JS functionality will alert the user if either input is not a value for input, to enter a valid response. Functionality and viewer presentation completed for 100-projects-038-bmi-calculator1 project -KS

--- 100-projects-038-bmi-calculator1.js
+++ 100-projects-038-bmi-calculator1.js
@@ -1,2 +1,17 @@
 'use strict'
 
+document.getElementById('calculate').addEventListener('click', () => {
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+    let result = document.getElementById('result');
+
+    if (weight === "" || isNaN(weight)) {
+        result.textContent = "Please enter a valid weight!";
+    } else if (height === "" || isNaN(height)) {
+        result.textContent = "Please enter a valid height!";
+    } else {
+        let bmi = (weight / (height * height / 10000)).toFixed(2);
+            result.textContent = "Your BMI is " + bmi;
+        }
+});
+
