492a5f7a517ba7ea6e8dbf2e07a50bf67e495b61
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-01 09:28:16 -0500

    Created function for checking password. Checks if empty/lowercase/uppercase/number/lenght min 8/ special characters. For each that is truthy, it added value of one to give a total betwwen 0-5. That value is then run through a created switch that evaluates the score to a password strength of /very weak/weak/medium/strong/very strong which will be presented in html for user. -KS

--- 100-projects-040-password-strength1.js
+++ 100-projects-040-password-strength1.js
@@ -1,2 +1,53 @@
 'use strict'
 
+function checkPasswordStrength(password) {
+    var strength = 0;
+
+    //Check length
+    if (password.length >= 8) {
+        strength++;
+    }
+
+    //Check for lowercase letters
+    if (/[a-z]/.test(password)) {
+        strength++
+    }
+
+    //Check for uppercase letters
+    if (/[A-Z]/.test(password)) {
+        strength++;
+    }
+
+    //Check for numbers
+    if (/[0-9]/.test(password)) {
+        strength++;
+    }
+
+    //Check for special characters
+    if (/[^a-zA-Z0-9]/.test(password)) {
+        strength++;
+    }
+
+    var passwordStrength = document.getElementById('passwordStrength');
+
+    switch (strength) {
+        case 0:
+            passowrdStrength.textContent = '';
+            break;
+        case 1:
+            passwordStrength.textContent = 'Very weak';
+            break;
+        case 2:
+            passwordStrength.textContent = 'Weak';
+            break;
+        case 3:
+            passwordStrength.textContent = 'Medium';
+            break;
+        case 4:
+            passwordStrength.textContent = 'Strong';
+            break;
+        case 5:
+            passwordStrength.textContent = 'Very strong';
+            break;
+    }
+}
