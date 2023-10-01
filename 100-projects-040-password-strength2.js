238a6c883b1d9908ca0ea681c7ba22e39a57c6c3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-01 10:42:02 -0500

    Added the same function that checks for password characters and evaluates a value from 100-projects-040-password-strength1 project. In addition, we added a variable of common passwords, which becomes a sixth case for checking that uses does not use most common passwords. Also added into the switch method within the function was each case was given a textcontent to the breakTime paragraph in html to display the estimated time a password could be broken into based on the determined strength, for the user to understand their password strength better -KS

--- 100-projects-040-password-strength2.js
+++ 100-projects-040-password-strength2.js
@@ -1,1 +1,70 @@
 'use strict'
+
+var commonPasswords = ['123456', 'password', '12345678', 'qwerty', 'abc123'];
+
+function checkPasswordStrength(password) {
+    var strength = 0;
+
+    //Check length
+    if (password.length >= 8) {
+        strength++;
+    }
+
+    //Check for lowercase letters
+    if (/[a-z]/.test(password)) {
+        strength++
+    }
+
+    //Check for uppercase letters
+    if (/[A-Z]/.test(password)) {
+        strength++;
+    }
+
+    //Check for numbers
+    if (/[0-9]/.test(password)) {
+        strength++;
+    }
+
+    //Check for special characters
+    if (/[^a-zA-Z0-9]/.test(password)) {
+        strength++;
+    }
+
+    //Check against list of common passwords
+    if (!commonPasswords.includes(password)) {
+        strength++;
+    }
+
+    var passwordStrength = document.getElementById('passwordStrength');
+    var breakTime = document.getElementById('breakTime');
+
+    switch (strength) {
+        case 0:
+            passowrdStrength.textContent = '';
+            break;
+        case 1:
+            passwordStrength.textContent = 'Very weak';
+            breakTime.textContent = "Estimated break time: Instant";
+            break;
+        case 2:
+            passwordStrength.textContent = 'Weak';
+            breakTime.textContent = "Estimated break time: Seconds";
+            break;
+        case 3:
+            passwordStrength.textContent = 'Medium';
+            breakTime.textContent = "Estimated break time: Hours";
+            break;
+        case 4:
+            passwordStrength.textContent = 'Strong';
+            breakTime.textContent = "Estimated break time: Days";
+            break;
+        case 5:
+            passwordStrength.textContent = 'Very strong';
+            breakTime.textContent = "Estimated break time: Years";
+            break;
+        case 6:
+            passwordStrength.textContent = 'Extremely Strong';
+            breakTime.textContent = "Estimated break time: Decades";
+            break;
+    }
+}
