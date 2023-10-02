ca1861580393d6a04bc04282a61f487fb9fb3dbe
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-02 06:16:02 -0500

    The generate function from 100-projects-041-fibonacci-sequence1 project was used in 100-projects-041-fibonacci-sequence2-bootstrap project. Function has been checked for functionality. Project completed. -KS

--- 100-projects-041-fibonacci-sequence2-bootstrap.js
+++ 100-projects-041-fibonacci-sequence2-bootstrap.js
@@ -1,2 +1,12 @@
 'use strict'
 
+function generateFibonacci() {
+    var num = document.getElementById('num').value;
+    var sequence = [0, 1];
+
+    while ((sequence[sequence.length - 1] + sequence[sequence.length - 2]) <= num) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+
+    document.getElementById('sequence').innerText = 'Fibonacci Sequence: ' + sequence.join(', ');
+}
