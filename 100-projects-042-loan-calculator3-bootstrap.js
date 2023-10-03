5ce9c8ed7b67b50b57113d748879b168e0e5d2be
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-03 13:13:45 -0500

    Finished 100-projects-042-loan-calculator3-bootstrap project. Finished html elements with bootstrap style classes. This calculator uses loan amount/ interet rate/grace period/repayment period  inputs to evaluate and return table of results from the javascript function. -KS

--- 100-projects-042-loan-calculator3-bootstrap.js
+++ 100-projects-042-loan-calculator3-bootstrap.js
@@ -1,2 +1,30 @@
-'ue strict'
+'use strict'
 
+function calculateLoan() {
+    let loanAmount = parseFloat(document.getElementById('loanAmount').value);
+    let interestRate = parseFloat(document.getElementById('interestRate').value) / 100 / 12;
+    let gracePeriod = parseFloat(document.getElementById('gracePeriod').value);
+    let repaymentPeriod = parseFloat(document.getElementById('repaymentPeriod').value);
+
+    let x = Math.pow(1 + interestRate, repaymentPeriod);
+    let monthlyPayment = (loanAmount * x * interestRate) / (x-1);
+    let totalPayment =  monthlyPayment * (gracePeriod + repaymentPeriod);
+    let totalInterest = totalPayment - loanAmount;
+
+    let output = `
+        <table class="table table-bordered">
+            <tr>
+                <th>Monthly Payment</th>
+                <th>Total Payment</th>
+                <th>Total Interest</th>
+            </tr>
+            <tr>
+                <td>$${monthlyPayment.toFixed(2)}</td>
+                <td>$${totalPayment.toFixed(2)}</td>
+                <td>$${totalInterest.toFixed(2)}</td>
+            </tr>
+        </table>
+    `;
+
+    document.getElementById("output").innerHTML = output;
+}
