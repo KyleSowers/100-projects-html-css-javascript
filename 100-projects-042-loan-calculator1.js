f9f712e42df579452e39ceee39bbe0af300bbeaf
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-03 05:46:16 -0500

    When user inputs the loan amount/ interest rate/ loan term...the button calls upon the calculateLoan() function. The values are grabbed, The calculation uses a formula for calculating loan payment, given the priciple amount, interest rate and payment period. The result is output to the user. -KS

--- 100-projects-042-loan-calculator1.js
+++ 100-projects-042-loan-calculator1.js
@@ -1,2 +1,11 @@
 'use strict'
 
+function calculateLoan() {
+    let loanAmount = document.getElementById('loanAmount').value;
+    let interestRate = document.getElementById('interestRate').value / 100 / 12;
+    let loanTerm = document.getElementById('loanTerm').value * 12;
+
+    let x =Math.pow(1 + interestRate, loanTerm);
+    let monthlyPayment = (loanAmount * x * interestRate) / (x -1);
+    document.getElementById('output').innerHTML = "Monthly Payment: $" + monthlyPayment.toFixed(2);
+}
