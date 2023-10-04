98e7f925c81fb1ede3f369e05ffec58148bbc905
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-04 07:55:19 -0500

    Created an async function to use input and call upon openweatherapi to get weather infomation for input of city from the user. Provides city name, temperature and weather. After checking for functionality, I removed my API for openweathermap until it is stored and imported safely. -KS

--- 100-projects-043-weather-app1.js
+++ 100-projects-043-weather-app1.js
@@ -1,2 +1,18 @@
 'use strict'
 
+async function getWeather() {
+    const city = document.getElementById('city').value;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=&units=metric`);
+    const data = await response.json();
+
+    if (response.ok) {
+        const weatherDiv = document.getElementById('weather');
+        weatherDiv.innerHTML = `
+            <h2>Weather in ${data.name}</h2>
+            <p><strong>Temperature</strong> ${data.main.temp}</p>
+            <p><storng>Weather</storng>${data.weather[0].description}</p>
+        `;
+    } else {
+        alert(data.message);
+    }
+}
