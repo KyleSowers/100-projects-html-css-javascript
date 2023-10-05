849502183fee6c1844e832950d2ca4af6c0e03dc
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-05 07:46:41 -0500

    Created the addTask function for 100-projects-044-todo-list project. User inputs something ToDo  and presses button. The function creates a new task object and pushes to the task array. Then, clears the input field and updates the task list. RenderTasks function was created clears task list, recreates and provides it in the form of an li item within the ul. It includes checkbox for marking completed (with toggle)  and a delete button. ToggleTask function toggles/updates the task. DeleteTask function, removes task from list. -KS

--- 100-projects-044-todo-list1.js
+++ 100-projects-044-todo-list1.js
@@ -1,2 +1,42 @@
 'use strict'
 
+let tasks = [];
+
+function addTask() {
+    const taskInput = document.getElementById('new-task');
+    const newTask = taskInput.value;
+
+    if (newTask) {
+        tasks.push({
+            text: newTask,
+            completed: false,
+        });
+        taskInput.value = '';
+        renderTasks();
+    }
+}
+
+function renderTasks() {
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.className = 'task' + (task.completed ? ' completed' : '');
+        li.innerHTML = `
+        <input type="checkbox" onchange="toggleTask(${index})" ${task.completed ? 'checked' : ''}>
+        ${task.text}
+        <button onclick="deleteTask(${index})">Delete</button>
+        `;
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+}
