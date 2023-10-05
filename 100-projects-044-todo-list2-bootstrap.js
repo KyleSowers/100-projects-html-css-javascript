fe8fae90ba90b0b3b8ccfd0e8b2e1836e2413b33
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-05 17:44:16 -0500

    Fixed missing functionality to add task from previously created functionality in 100-projects-044-todo-list1 project. Then discovered need for missing bootstrap property of "list group item success" within the created list item, which prevented the functionality of a checked list item to be highlighted when selected for completion. -KS

--- 100-projects-044-todo-list2-bootstrap.js
+++ 100-projects-044-todo-list2-bootstrap.js
@@ -1,1 +1,45 @@
 'use strict'
+
+let tasks = [];
+
+function addTask() {
+    const taskInput = document.getElementById('new-task');
+    const newTask = taskInput.value;
+
+    if (newTask) {
+        tasks.push({
+            text: newTask,
+            completed: false,
+        });
+        taskInput.value = '';
+        renderTasks();
+    }
+}
+function renderTasks() {
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center' + (task.completed ? ' list-group-item-success' : '');
+        li.innerHTML = `
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" onchange="toggleTask(${index})" ${task.completed ? 'checked' : ''}>
+                <label class="form-check-label ${task.completed ? 'text-decoration-line-through' : ''}">
+                ${task.text}
+                </label>
+            </div>
+            <button class="btn btn-danger btn-sm" onclick="deleteTask(${index})">Delete</button>
+        `;
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+}
