bebb3a4bb51503e78abb954d9b53141546651c2f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-06 08:45:20 -0500

    Utilized the same calculateTax() function as 100-projects-045-tax-calculator2-boostrap project. Get cost and tax rate input from user, then calcualtes the tax amount and new total for user. -KS

--- 100-projects-045-tax-calculator2-bootstrap.js
+++ 100-projects-045-tax-calculator2-bootstrap.js
@@ -1,2 +1,13 @@
 'use strict'
 
+function calculateTax() {
+    const cost = parseFloat(document.getElementById('cost').value);
+    const taxRate = parseFloat(document.getElementById('tax-rate').value) / 100;
+    const tax = cost * taxRate;
+    const total = cost + tax;
+    const output = document.getElementById('output');
+    output.innerHTML = `
+        <p><strong>Tax:</strong> $${tax.toFixed(2)}</p>
+        <p><strong>Total:</strong> $${total.toFixed(2)}</p>
+    `;
+}
