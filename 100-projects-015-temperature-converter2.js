27b37774c410d83ba444b954c3e7fd6b92b141f1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-07 09:34:00 -0500

    Updates 100-projects-015 1 & 2 projects, created short active video and added to master-100-projects bootstrap carosel. -KS

--- 100-projects-015-temperature-converter2.js
+++ 100-projects-015-temperature-converter2.js
@@ -5,9 +5,9 @@ function convertTemperature() {
     let output = document.getElementById('output');
 
     if (document.getElementById('f_to_c').checked) {
-        output.innerHTML = ((temp -32) * 5 / 9).toFixed(2);
+        output.innerHTML = ((temp -32) * 5 / 9).toFixed(2) + '°C';
     } else if (document.getElementById('c_to_f').checked) {
-        output.innerHTML = ((temp * 9 / 5) + 32).toFixed(2);
+        output.innerHTML = ((temp * 9 / 5) + 32).toFixed(2) + '°F';
     } else {
         output.innerHTML = "Please select conversion method.";
     }
