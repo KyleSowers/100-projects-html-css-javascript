d5bb81781e5869644d7267a8d365f85b20e9a27e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-07 11:02:56 -0500

    Updated background color of 100-projects-016 #2. Becuase the image shows two dice for dice roller and it is common to roll two dice, I updated the function to find two dice values, add together, and return total of two dice roll to user. Then, becuase project originally displayed only one of the 45 numbers at a time to user for choosing random lottery numbers but displayed 6/45 as a set of full lottery selection, I added a series of do while loops which found additional numbers, compared to make sure each new number was not already chosen, then added a string of the results toa variable and returned result of all six needed random lottery numbers to user with one click. -KS

--- 100-projects-016-dice-lotto-roll2.js
+++ 100-projects-016-dice-lotto-roll2.js
@@ -2,10 +2,41 @@
 
 document.getElementById('roll-dice-button').onclick =function () {
     let result = Math.floor(Math.random() * 6) +1;
-    document.getElementById('result').innerHTML = result;
+    let result2 = Math.floor(Math.random() * 6) +1;
+    let twoDiceRoll = result2 + result;
+    document.getElementById('result').innerHTML = twoDiceRoll;
 };
 
 document.getElementById('roll-dice-button2').onclick = function () {
     let result = Math.floor(Math.random() * 45) +1;
-    document.getElementById('result2').innerHTML = result;
+
+    let result2;
+    let result3;
+    let result4;
+    let result5;
+    let result6;
+
+    do {
+        result2 = Math.floor(Math.random() *45) +1;
+    } while (result2 === result);
+
+    do {
+        result3 = Math.floor(Math.random() * 45) +1;
+    } while (result3 === result || result3 === result2);
+
+    do {
+        result4 = Math.floor(Math.random() * 45) +1;
+    } while (result4 === result || result4 === result2 || result4 === result3);
+
+    do {
+        result5 = Math.floor(Math.random() * 45) +1;
+    } while (result5 === result || result5 === result2 || result5 === result3 || result5 === result4);
+
+    do {
+        result6 = Math.floor(Math.random() * 45) +1;
+    } while (result6 === result || result6 === result2 || result6 === result3 || result6 === result4 || result6 === result5);
+
+    let fullLottoResult = `${result}, ${result2}, ${result3}, ${result4}, ${result5}, ${result6}`;
+
+    document.getElementById('result2').innerHTML = fullLottoResult;
 };
