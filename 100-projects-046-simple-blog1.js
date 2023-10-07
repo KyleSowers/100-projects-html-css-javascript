932996bc6bbd02844724e1b508c37a6cf690d926
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-07 15:41:49 -0500

    Created a function to add comment from user. It takes the input stores value into a new variable, then pushes new content into a created comments array. the original content variable is reset to empty and the function calls on the array. The render comments function was created to create a div and take array of comments to display in array. When a comment is added, it is added to the array and to the div. -KS

--- 100-projects-046-simple-blog1.js
+++ 100-projects-046-simple-blog1.js
@@ -1,5 +1,24 @@
 'use strict'
 
-function addComment() {
+let comments = [];
 
+function addComment() {
+    const commentInput = document.getElementById('comment-input');
+    const newComment = commentInput.value;
+        if (commentInput) {
+            comments.push(newComment);
+            commentInput.value = "";
+            renderComments();
+        }
+}
+
+function renderComments() {
+    const commentsDiv = document.getElementById('comments');
+    commentsDiv.innerHTML = "";
+        comments.forEach(comment => {
+        const div =document.createElement('div');
+        div.className = 'comment';
+        div.textContent = comment;
+        commentsDiv.appendChild(div);
+    });
 }
