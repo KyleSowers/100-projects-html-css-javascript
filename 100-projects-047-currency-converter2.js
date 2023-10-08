b52fb209f421a53133944f85e20907675e277c1f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-08 14:43:15 -0500

    Added the 100-projects-047-currency-converter1 project async function convertCurrency() api fetcher. Added after variable which stores the coverted amount, a formattedAmount variable which creates a new INTL number format to format numbers according to specific locale settings. Set to en-US. Functionality was tested with personal API key and was removed for security from git push until stored into a safe file.-KS

--- 100-projects-047-currency-converter2.js
+++ 100-projects-047-currency-converter2.js
@@ -1,2 +1,19 @@
 'use strict'
 
+async function convertCurrency() {
+    const amount = parseFloat(document.getElementById('amount').value);
+    const fromCurrency = document.getElementById('from-currency').value.toUpperCase();
+    const toCurrency = document.getElementById('to-currency').value.toUpperCase();
+
+//     http://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/USD create own number.
+    const responce = await fetch(`https://v6.exchangerate-api.com/v6/YOURKEYHERE/latest/${fromCurrency}`);
+    const data = await responce.json();
+    if(data && data.conversion_rates) {
+        const rate = data.conversion_rates[toCurrency];
+        const convertedAmount = amount * rate;
+        const formattedAmount = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2}).format(convertedAmount);
+        document.getElementById('output').innerHTML = `${amount} ${fromCurrency} = ${formattedAmount} ${toCurrency}`;
+    } else {
+        document.getElementById('output').innerHTML = 'An error occurred while fetching the exchange rates';
+    }
+}
