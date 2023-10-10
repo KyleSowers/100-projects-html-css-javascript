d1af9c2c25fe71ed40eca6bed5f47aeb3f070483
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-10 09:09:32 -0500

    Began JS -KS

--- 100-projects-049-mad-libs-game2.js
+++ 100-projects-049-mad-libs-game2.js
@@ -1,2 +1,16 @@
 'use strict'
 
+var stories = [
+        "A lazy {{noun}} named Whikers lived in a small village. He lovd {{verb}} the world go by from his {{adjective}} cushion. " +
+    "One day, he saw a {{noun}} scurrying across the floor."
+    "{{adverb}}, Whiskers chased the {{noun}}, enjoying a thrilling game of chase. To his surprise, Whiskers discovered he loved  the excitement. From that day, he decided to be an {{adjective}} {{noun}}.",
+
+    "In a bustling city, a lonely {{noun}} named Percy lived atop a tall building. Percy watched children {{verb}} in the park, wishing he had a friend. One day, he gathered courage and {{verb}} down, introducing himself to a little {{noun}}. She named him 'buddy', and they became {{adjective}} friends, sharing sandwiches and giggles {{adverb}}.",
+
+    "{{noun}}, a curious {{noun}}, lived in a small tank. She often wondered about life beyond her glass walls. One day, her owner left the lid open. {{adverb}}, {{noun}} jumped out, landing in a bigger tank. She was scared but {{adverb}}. She realized a bigger world waited for her, full of {{adjective}} wonders",
+
+    "Benny, a brave {{noun}}, lived in a  peaceful countryside farm. He spent his days {{verb}} sheep and guarding the {{noun}}. One chilly night, he heard a whimpering sound from the woods. Benny found a lost {{noun}}, shivering and scared. He guided the {{noun}} home, and they became {{adjective}}. Benny realized he was not just a guard {{noun}}, but a {{adjective}} protector.",
+    "Oliver, an owl, loved the tranquility of night. He adored the twinkling stars and the rustling leaves. However, he felt alone, as everyone else slept. One night, he met Luna,"
+
+
+]
