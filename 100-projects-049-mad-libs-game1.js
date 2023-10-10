67be24fae636a47beb8f3ef2e071f18425674eb7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-10 07:37:21 -0500

    Created the function generateStory(), called upon inputs for values of noun, verb, adjective, and adverb, created story string stored in variable named story, called variable story into div for output in html. -KS

--- 100-projects-049-mad-libs-game1.js
+++ 100-projects-049-mad-libs-game1.js
@@ -1,2 +1,14 @@
 'use strict'
 
+function generateStory() {
+    var noun = document.getElementById('noun').value;
+    var verb = document.getElementById('verb').value;
+    var adjective = document.getElementById('adjective').value;
+    var adverb = document.getElementById('adverb').value;
+
+    var story = `Once upon a time, there was a very ${adjective} ${noun}. The ${noun} was always ${verb} ${adverb} in the forest.`;
+
+    document.getElementById('story').innerHTML = story;
+
+    document.getElementById('madlib-story').style.display = 'block';
+}
