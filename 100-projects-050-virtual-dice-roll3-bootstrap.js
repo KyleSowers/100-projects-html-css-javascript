2fc29652a76de95daf7dca08f1f45b6833fb614f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-11 08:36:50 -0500

    JavaScript function input. Error is restricing the function, will need to be worked out later today. Added a history div/ class which will display past rolls. -KS

--- 100-projects-050-virtual-dice-roll3-bootstrap.js
+++ 100-projects-050-virtual-dice-roll3-bootstrap.js
@@ -1,2 +1,12 @@
 'use strict'
 
+${document}.ready(function () {
+    $('#roll-button').click(function () {
+        var dice = Math.floor(Math.random() * 6) +1;
+        ${"dice"}.attr("src", 'dice_no' + dice + '.jpg');
+
+    //     Add roll result to history
+        var entry = $('div').addclass('history-entry').text('You rolled a' + dice);
+        $('#history').append(entry);
+    })
+})
