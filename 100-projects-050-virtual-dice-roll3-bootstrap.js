3a79e008c7c90984d180eeff2a1ac5c190657571
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-12 20:42:57 -0500

    Added the correct photos to reveal user random dice roll like 100-projects-050-virtual-dice-roll2 project. Also fixed errors in the function from {}'s to ()'s and text error limiting the ability to function. Project now displays the result and a history of dice rolls. -KS

--- 100-projects-050-virtual-dice-roll3-bootstrap.js
+++ 100-projects-050-virtual-dice-roll3-bootstrap.js
@@ -1,12 +1,12 @@
 'use strict'
 
-${document}.ready(function () {
+$(document).ready(function () {
     $('#roll-button').click(function () {
-        var dice = Math.floor(Math.random() * 6) +1;
-        ${"dice"}.attr("src", 'dice_no' + dice + '.jpg');
+        var dice = Math.floor(Math.random() * 6) + 1;
+        $("#dice").attr("src", 'images/dice_no' + dice + '.png');
 
     //     Add roll result to history
-        var entry = $('div').addclass('history-entry').text('You rolled a' + dice);
+        var entry = $('<div>').addClass('history-entry').text('You rolled a' + dice);
         $('#history').append(entry);
     })
 })
