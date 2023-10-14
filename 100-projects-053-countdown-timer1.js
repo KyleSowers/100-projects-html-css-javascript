030741d05476ac42a755b78959875338a7dd16f1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-14 09:39:19 -0500

    created a function to set a variable representing set interval, add event to the user button to check if user integer value input was an integer, with included parse option. Then the input is evaluated, will return if NaN or empty. If integer value passes, timer will activate. The function checks for timer value to represent 00 to clear the interval and finally is set to equate the operator to check and append the leading 0 if value of active timer is below the number ten. -KS

--- 100-projects-053-countdown-timer1.js
+++ 100-projects-053-countdown-timer1.js
@@ -1,2 +1,23 @@
 'use strict'
 
+let countdown; //variable to hold set interval
+
+document.getElementById('start-countdown').addEventListener('click', function() {
+    let timeInput = document.getElementById('time-input').value; //Users input
+    if (!timeInput) return; // Return if input is empty
+
+    timeInput = parseInt(timeInput);
+    if (isNaN(timeInput) || timeInput < 0) return; // Retrun if input is not a positive number
+
+    if (countdown) clearInterval(countdown); // If a countdown is already active, clear it
+
+    countdown = setInterval(function () {
+        if (timeInput <= 0) { // If the countdown should be over
+            clearInterval(countdown); // Clear the countdown
+            document.getElementById('timer').textContent = '00'; // Reset the displayed countdown
+        } else {
+            timeInput--; // Subtract one second
+            document.getElementById('timer').textContent = (timeInput > 9 ? "" : "0") + timeInput; // Display new time
+        }
+    }, 1000); // Run the function every second
+});
