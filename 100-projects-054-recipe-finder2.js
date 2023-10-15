3eac6c6e12bd76e9a467d40f4512edd127abecb9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-15 09:30:41 -0500

    Added the javascript functioning, including the api recipe fetch.

--- 100-projects-054-recipe-finder2.js
+++ 100-projects-054-recipe-finder2.js
@@ -1,2 +1,29 @@
 'use strict'
 
+document.getElementById('search-recipes').addEventListener('click', function (){
+    let ingredientInput = document.getElementById('ingredient-input').value;
+    if (!ingredientInput) return;
+
+    const ingredients = ingredientInput.split(',').map(ingredient => ingredient.trim());
+
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients.join(',')}&apiKey=YOURAPIKEY`)
+        .then(response => response.json())
+        .then(data => {
+            const recipesElement = document.getElementById('recipes');
+            recipesElement.innerHTML = '';
+
+            data.forEach(recipe => {
+                const recipeElement = document.creeateElement('div');
+                recipeElement.classList.add('recipe');
+
+                recipeElement.innerHTML = `
+                <h2>${recipe.title}</h2>
+                <img src='${recipe.image}' alt='${recipe.title}'/>
+                <p>${recipe.usedIngredientCount} ingredients used: ${recipe.usedIngredients.map(i => name).join(',')}</p>
+                <p>${recipe.missedIngredientCount} ingredients missed: ${recipe.missedIngredients.map(i => i.name).join(',')}</p>
+                `;
+                recipesElement.appendChild(recipeElement);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+});
