73966b55ad827ebe238997ba92411a59c6f517ec
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-16 14:26:58 -0500

    Called on button to add event with const(s) of bill/tip/people values stored in variables. If any input isNaN than the function returns asking for valid input to user.

--- 100-projects-055-bill-splitter1.js
+++ 100-projects-055-bill-splitter1.js
@@ -1,2 +1,12 @@
 'use strict'
 
+document.getElementById('calculate-tip').addEventListener('click', function () {
+    const bill = parseFloat(document.getElementById('bill-input').value);
+    const tipPercentage = parseFloat(document.getElementById('tip-input').value);
+    const people = parseInt(document.getElementById('people-input').value);
+
+    if (isNaN(bill) || isNaN(tipPercentage) || isNaN(people) || bill < 0 || tipPercentage < 0 || people < 1) {
+        document.getElementById('result').textContent = "Please enter valid inputs!";
+        return;
+    }
+})
