891654081b2b5bd5e03e173a61b245d6d3845355
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-16 20:28:45 -0500

    Corrected js error

--- 100-projects-055-bill-splitter3-bootstrap.js
+++ 100-projects-055-bill-splitter3-bootstrap.js
@@ -3,7 +3,7 @@
 $('#calculate-tip').click(function () {
     const bill = parseFloat($('#bill-input').val());
     const tipPercentage = parseFloat($('#tip-input').val());
-    const people = parseInt($('people-input').val());
+    const people = parseInt($('#people-input').val());
 
     if (isNaN(bill) || isNaN(tipPercentage) || isNaN(people) || bill < 0 || tipPercentage < 0 || people < 1) {
         $('#result').text('Please enter valid inputs!');
