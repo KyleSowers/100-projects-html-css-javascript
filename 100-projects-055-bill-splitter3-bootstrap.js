9ebb9a186062baab243e7ce1989bbcdbface5fdf
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-16 20:25:05 -0500

    Altered previous js functionality for bill splitter to jquery.

--- 100-projects-055-bill-splitter3-bootstrap.js
+++ 100-projects-055-bill-splitter3-bootstrap.js
@@ -1,2 +1,18 @@
 'use strict'
 
+$('#calculate-tip').click(function () {
+    const bill = parseFloat($('#bill-input').val());
+    const tipPercentage = parseFloat($('#tip-input').val());
+    const people = parseInt($('people-input').val());
+
+    if (isNaN(bill) || isNaN(tipPercentage) || isNaN(people) || bill < 0 || tipPercentage < 0 || people < 1) {
+        $('#result').text('Please enter valid inputs!');
+        return;
+    }
+
+    const tipAmount = bill * (tipPercentage / 100);
+    const totalAmount = bill + tipAmount;
+    const splitAmount = totalAmount / people;
+
+    $('#result').text(`Total bill with tip: $${totalAmount.toFixed(2)}\nEach person should pay: $${splitAmount.toFixed(2)}`);
+});
