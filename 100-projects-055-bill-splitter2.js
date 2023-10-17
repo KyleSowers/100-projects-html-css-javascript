768fe9ac51de8b405156c6faa88482aa692c091b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-16 19:43:37 -0500

    Event listener added to button. all inputs are parsed and checked for validity. If no, message returned for valid input. If valid methods for tip, total and split amount are run to return values back to the user.

--- 100-projects-055-bill-splitter2.js
+++ 100-projects-055-bill-splitter2.js
@@ -1,2 +1,18 @@
 'use strict'
 
+document.getElementById('calculate-tip').addEventListener('click', function () {
+    const bill = parseFloat(document.getElementById('bill-input').value);
+    const tipPercentage = parseFloat(document.getElementById('tip-input').value);
+    const people = parseInt(document.getElementById('people-input').value);
+
+    if (isNaN(bill) || isNAN(tipPercentage) || isNAN(people) || bill < 0 || tipPercentage < 0  || people < 1 ) {
+        document.getElementById('result').textContent = "Please enter valid inputs!";
+        return;
+    }
+
+    const tipAmount = bill * (tipPercentage / 100);
+    const totalAmount = bill + tipAmount;
+    const splitAmount = totalAmount / people;
+
+    document.getElementById('result').textContent = `Total bill with tip: $${totalAmount.toFixed(2)}\nEach person should pay: $${splitAmount.toFixed(2)}`;
+});
