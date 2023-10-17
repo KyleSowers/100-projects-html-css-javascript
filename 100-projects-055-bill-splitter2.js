28f642158f582bc93b9ff0bf4943c61a2b14ecf2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-16 20:00:25 -0500

    Completed CSS properties. Creates a great visual container display of a soft dark green background. Container stands off background with offset boxshadow.

--- 100-projects-055-bill-splitter2.js
+++ 100-projects-055-bill-splitter2.js
@@ -5,7 +5,7 @@ document.getElementById('calculate-tip').addEventListener('click', function () {
     const tipPercentage = parseFloat(document.getElementById('tip-input').value);
     const people = parseInt(document.getElementById('people-input').value);
 
-    if (isNaN(bill) || isNAN(tipPercentage) || isNAN(people) || bill < 0 || tipPercentage < 0  || people < 1 ) {
+    if (isNaN(bill) || isNaN(tipPercentage) || isNaN(people) || bill < 0 || tipPercentage < 0  || people < 1 ) {
         document.getElementById('result').textContent = "Please enter valid inputs!";
         return;
     }
