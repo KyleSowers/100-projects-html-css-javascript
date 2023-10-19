77c21d29f994cdb43cc8e917e1f3ff3134d65c29
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-18 21:47:10 -0500

    Added the same langauge array from previous example exercise. Reformatted the same two functions, but with use of jquery.

--- 100-projects-057-new-language-cipher2-bootstrap.js
+++ 100-projects-057-new-language-cipher2-bootstrap.js
@@ -1,2 +1,36 @@
 'use strict'
 
+const NewLanguage = {
+    'a': '!', 'b': '@', 'c': '#', 'd': '$', 'e': '%', 'f': '^', 'g': '&', 'h': '*', 'i': '(', 'j': ')', 'k':
+        '-', 'l': '=', 'm': '+', 'n': '[', 'o': ']', 'p': '{', 'q': '}', 'r': ';', 's': ':', 't': ',', 'u': '.', 'v': '<', 'w': '>', 'x': '/', 'y': ':', 'z': '|', '': '~', '!': 'x'};
+
+const inverseNewLanguage = {};
+for (const key in NewLanguage) {
+    inverseNewLanguage[NewLanguage[key]] = key
+}
+
+$('#to-new-language').click(function () {
+    const inputEnglish = $('#input-english').val().toLowerCase();
+    let outputNewLanguage = '';
+    for (let i = 0; i < inputEnglish.length; i++) {
+        if (NewLanguage[inputEnglish[i]]) {
+            outputNewLanguage += NewLanguage[inputEnglish[i]];
+        } else {
+            outputNewLanguage += '?';
+        }
+    }
+    $('#input-new-language').val(outputNewLanguage);
+});
+
+$('#to-english').click(function () {
+    const inputNewLanguage = $('#input-new-language').val();
+    let outputEnglish = '';
+    for ( let i = 0; i < inputNewLanguage.length; i++) {
+        if (inverseNewLanguage[inputNewLanguage[i]]) {
+            outputEnglish += inverseNewLanguage[inputNewLanguage[i]];
+        } else {
+            outputEnglish += '?';
+        }
+    }
+    $('#input-english').val(outputEnglish);
+})
