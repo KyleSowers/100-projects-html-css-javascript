13f656e31e276cb87ec69fbb2e106c44a258ded7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-18 21:12:18 -0500

    Added an array for a new language based on current letters and symbols. A function to read english input and output new language. A second function to read and inverse the new language into English.

--- 100-projects-057-new-language-cipher1.js
+++ 100-projects-057-new-language-cipher1.js
@@ -1,2 +1,31 @@
 'use strict'
 
+const NewLanguage = {
+    'a': '!', 'b': '@', 'c': '#', 'd': '$', 'e': '%', 'f': '^', 'g': '&', 'h': '*', 'i': '(', 'j': ')', 'k':
+'-', 'l': '=', 'm': '+', 'n': '[', 'o': ']', 'p': '{', 'q': '}', 'r': ';', 's': ':', 't': ',', 'u': '.', 'v': '<', 'w': '>', 'x': '/', 'y': ':', 'z': '|', '': '~', '!': 'x'};
+
+document.getElementById('to-new-language').addEventListener('click', function () {
+    const inputEnglish = document.getElementById('input-english').value.toLowerCase();
+    let outputNewLanguage = '';
+    for (let i = 0; i < inputEnglish.length; i++) {
+        if (NewLanguage[inputEnglish[i]]) {
+            outputNewLanguage += NewLanguage[inputEnglish[i]] + '';
+        } else {
+            outputNewLanguage += '?';
+        }
+    }
+    document.getElementById('input-new-language').value = outputNewLanguage;
+})
+
+document.getElementById('to-english').addEventListener('click', function () {
+    const inputNewLanguage = document.getElementById('input-new-language').value;
+    let outputEnglish = '';
+    for (let i = 0; i < inputNewLanguage.length; i++) {
+        if (inverseNewLanguage[inputNewLanguage[i]]) {
+            outputEnglish += inverseNewLanguage[inputNewLanguage[i]];
+        } else {
+            outputEnglish += '?';
+        }
+    }
+    document.getElementById('input-english').value = outputEnglish;
+});
