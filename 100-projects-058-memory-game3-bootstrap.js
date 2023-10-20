2e1a8ac329c9ee742fabed93a5090c32fad760b7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-19 21:15:48 -0500

    Added javascript but refactored to jquery for this version of the memory game.

--- 100-projects-058-memory-game3-bootstrap.js
+++ 100-projects-058-memory-game3-bootstrap.js
@@ -1,2 +1,47 @@
 'use strict'
 
+let cards = ['Lion', 'Tiger', 'Bear', 'Elephant', 'pig', 'cat', 'dog', 'cow', 'Lion', 'Tiger', 'Bear', 'Elephant', 'pig', 'cat', 'dog', 'cow'];
+let firstCard = null;
+let secondCard = null;
+let pairsFound = 0;
+
+function shuffleAndDealCards() {
+    cards.sort(() => 0.5 - Math.random());
+    $('#game-board').empty();
+
+    $.each(cards, (i, cardText) => {
+        let cardelement = $(`<div class="card"><div>${cardText}</div></div>`);
+        $(`#game-board`).append(cardelement);
+
+        cardelement.on('click', function () {
+            if (!firstCard) {
+                firstCard = cardelement;
+                firstCard.children().show();
+            } else if (!secondCard) {
+                secondCard = cardelement;
+                secondCard.children().show();
+
+                if (firstCard.text() === secondCard.text()) {
+                    firstCard = secondCard = null;
+                    pairsFound++;
+                    if (pairsFound === cards.length / 2) {
+                        alert('Congratulations! You found all pairs.');
+                    }
+                } else {
+                    setTimeout(function () {
+                        firstCard.children().hide();
+                        secondCard.children().hide();
+                        firstCard = secondCard = null;
+                    }, 1000);
+                }
+            }
+        });
+    });
+}
+
+$('#reset-button').on('click', fuction () {
+    pairsFound = 0;
+    shuffleAndDealCards();
+});
+
+shuffleAndDealCards();
