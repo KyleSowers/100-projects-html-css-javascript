9a5b63269417673d126092cfe3f27574ea73034f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-20 19:42:26 -0500

    Added a drawboard & makeMove function.

--- 100-projects-059-tic-tac-toe-game2.js
+++ 100-projects-059-tic-tac-toe-game2.js
@@ -18,3 +18,26 @@ function resetBoard() {
     drawBoard();
 }
 
+function  drawBoard() {
+    document.getElementById('board').innerHTML = '';
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            document.getElementById('board').innerHTML += `<div onclick="makeMove(${i}, ${j})">${board[i][j]}</div>`;
+        }
+    }
+}
+
+function makeMove() {
+    if (board[i][j] !== '') return;
+    board[i][j] = currentPlayer;
+    if (checkWin(currentPlayer)) {
+        setTimeout(() => {
+            alert(`Player ${currentPlayer} wins!`);
+            resetBoard();
+        }, 100);
+        return;
+    }
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    drawBoard();
+}
+
