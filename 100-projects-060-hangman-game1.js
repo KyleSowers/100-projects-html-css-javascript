24a088c5c9cbe9034445690f8ee21415f0e66c22
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-21 17:15:46 -0500

    Created functions resetGame/drawKeyboard/makeGuess to also utilize an array of hangman words to select onw at random, then use a created keyboard of letters for user selection by way of CharCode.

--- 100-projects-060-hangman-game1.js
+++ 100-projects-060-hangman-game1.js
@@ -1,2 +1,55 @@
 'use strict'
 
+let words = [
+    'script', 'style', 'html', 'css', 'coding', 'jquery', 'bootstrap', 'beginner'
+];
+let word = '';
+let answerArray = [];
+let remainingLetters = 0;
+
+function resetGame() {
+    word = words[Math.floor(Math.random() * words.length)];
+    answerArray = [];
+    for (let i = 0; i < word.length; i++) {
+        answerArray[i] = '_';
+    }
+    remainingLetters = word.length;
+    document.getElementById('word-container').innerText = answerArray.join(' ');
+    drawKeyboard();
+}
+
+function drawKeyboard() {
+    let keyboard = document.getElementById('keyboard');
+    keyboard.innerHTML = '';
+    for (let i = 65; i <= 90; i++) {
+        let letter = String.fromCharCode(i).toLowerCase();
+        let letterElement = document.createElement('div');
+        letterElement.innerText = letter;
+        letterElement.onclick = function() {
+            makeGuess(letter);
+        };
+        keyboard.appendChild(letterElement);
+    }
+}
+
+function makeGuess(letter) {
+    let isCorrectGuess = false;
+    for (let j = 0; j < word.length; j++) {
+        if (word[j] === letter && answerArray[j] === '_') {
+            answerArray[j] = letter;
+            remainingLetters--;
+            isCorrectGuess = true;
+        }
+    }
+    if(isCorrectGuess) {
+        document.getElementById('word-container').innerText = answerArray.joint('');
+    }
+    if (remainingLetters === 0) {
+        setTimeout(() => {
+            alert('Congratulations! The word was ' + word);
+            resetGame();
+        }, 100);
+    }
+}
+
+resetGame();
