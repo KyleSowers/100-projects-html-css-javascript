0070a1ac303bf2d84d6a5b1265f713835bf04ed3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-20 19:08:16 -0500

    Created a makeMove function.

--- 100-projects-059-tic-tac-toe-game1.js
+++ 100-projects-059-tic-tac-toe-game1.js
@@ -26,3 +26,17 @@ function drawBoard() {
         }
     }
 }
+
+function makeMove(i, j) {
+    if (board[i][j] !== '') return;
+    board[i][j] = currentPlayer;
+    if (checkWin(currentPlayer)) {
+        setTimeout(() => {
+            alert(`Player ${currentPlayer} wins!`);
+            resetBoard();
+        }, 100);
+        return;
+    }
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    drawBoard();
+}
