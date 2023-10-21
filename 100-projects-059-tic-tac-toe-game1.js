2822c1a8236f7c4d624bdc4ecf7cf303daff2cba
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-20 19:14:49 -0500

    Created checkwin function for game

--- 100-projects-059-tic-tac-toe-game1.js
+++ 100-projects-059-tic-tac-toe-game1.js
@@ -40,3 +40,14 @@ function makeMove(i, j) {
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     drawBoard();
 }
+
+function checkWin(player) {
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] === player && board[i][1] === player && board[i][2] === player) return true;
+        if (board[0][i] === player && board[1][i] === player && board[2][i] === player) return true;
+    }
+    if (board[0][0] === player && board[1][1] === player && board[2][2] === player) return true;
+    if (board[0][2] === player && board[1][1] === player && board[2][0] === player) return true;
+    return false;
+}
+drawBoard();
