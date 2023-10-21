995507943610de26323d1b1a7f8824fbb7c0b3dc
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-20 19:03:07 -0500

    functions resetboard and drawBoard created.

--- 100-projects-059-tic-tac-toe-game1.js
+++ 100-projects-059-tic-tac-toe-game1.js
@@ -7,3 +7,22 @@ let board = [
 ];
 
 let currentPlayer = 'X';
+
+function resetBoard() {
+    board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    currentPlayer = 'X';
+    drawBoard();
+}
+
+function drawBoard() {
+    document.getElementById('board').innerHTML = '';
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            document.getElementById('board').innerHTML += `<div onclick="makeMove(${i}, ${j})">${board[i][j]}</div>`;
+        }
+    }
+}
