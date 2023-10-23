3e44e7a703328321df05378daff2c35c24d5ffca
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-22 20:32:53 -0500

    Created drawPlanner function, but instead of 24 hour blocks to create, this planner is set up with an array of days of the week and will dusplay such to user.

--- 100-projects-061-daily-planner2.js
+++ 100-projects-061-daily-planner2.js
@@ -1,2 +1,45 @@
 'use strict'
 
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let today = new Date();
+let startOfWeek = today.getDate() - today.getDay();
+let planner = document.getElementById('planner');
+
+function drawPlanner() {
+    for (let i = 0; i < 7; i++) {
+        let dayDate = new Date(today.setDate(startOfWeek + i));
+        let dayBlock = document.createElement('div');
+        dayBlock.classList.add('day-block');
+
+        let dayInfo = document.createElement('div');
+        dayInfo.classList.add  ('day-info');
+
+        let dayTitle = document.createElement('h3');
+        dayTitle.innerText = days[i];
+
+        let dayDateElement = document.createElement('span');
+        dayDateElement.innerText = dayDate.toLocaleDateString();
+
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = function () {
+            textarea.value = '';
+            localStorage.removeItem(`plans-${dayDate.toLocaleDateString()}`);
+        };
+        dayInfo.appendChild(dayTitle);
+        dayInfo.appendChild(dayDateElement);
+        dayInfo.appendChild(deleteButton);
+
+        let textarea = document.createElement('textarea');
+        textarea.value = localStorage.getItem(`plans-${dayDate.toLocaleDateString()}`) || '';
+        textarea.onchange = function (event) {
+            localStorage.setItem(`plans-${dayDate.toLocaleDateString()}`, event.target.value);
+        };
+
+        dayBlock.appendChild(dayinfo);
+        dayBlock.appendChild(textarea);
+        dayBlock.appendChild(dayBlock);
+    }
+}
+
+drawPlanner();
