3f08564a92412fedb4fc9f5159b77cd998ecfb1a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-22 20:08:06 -0500

    Added CSS style properties for sizing and positioning.

--- 100-projects-061-daily-planner1.js
+++ 100-projects-061-daily-planner1.js
@@ -10,7 +10,7 @@ function drawPlanner() {
         hourBlock.classList.add('hour-block');
         let timeElement = document.createElement('span');
         timeElement.innerText = i < 10 ? `0${i}:00` : `${i}:00`;
-        let inputElement = document.createElement('inpput');
+        let inputElement = document.createElement('input');
         inputElement.value = planner[i];
         inputElement.onchange = function (event) {
             planner[i] = event.target.value;
