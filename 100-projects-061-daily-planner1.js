c0f1cfa2440e638b9252f1951112dd67a6a6518c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-22 20:00:37 -0500

    Created the drawPlanner function. Creates 24 blocks for the hours of the day. It loops over the 24 hours and appends an hour block for each hour. An onchange event allows the user to make changes. Delete onclick allows user to delete and refresh block.

--- 100-projects-061-daily-planner1.js
+++ 100-projects-061-daily-planner1.js
@@ -1,2 +1,31 @@
 'use strict'
 
+let planner = new Array(24).fill('');
+
+function drawPlanner() {
+    let plannerElement = document.getElementById('planner');
+    plannerElement.innerHTML = '';
+    for (let i = 0; i < 24; i++) {
+        let hourBlock = document.createElement('div');
+        hourBlock.classList.add('hour-block');
+        let timeElement = document.createElement('span');
+        timeElement.innerText = i < 10 ? `0${i}:00` : `${i}:00`;
+        let inputElement = document.createElement('inpput');
+        inputElement.value = planner[i];
+        inputElement.onchange = function (event) {
+            planner[i] = event.target.value;
+        };
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = function () {
+            planner[i] = '';
+            drawPlanner();
+        };
+        hourBlock.appendChild(timeElement);
+        hourBlock.appendChild(inputElement);
+        hourBlock.appendChild(deleteButton);
+        plannerElement.appendChild(hourBlock);
+    }
+}
+
+drawPlanner();
