f2afce888b36aa7aa9275e6c8bdc7ae455415373
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-24 11:53:38 -0500

    fixed errors in javascript preventing find more button, menu-icon & note-icon from functioning properly. Project completed.

--- 100-projects-063-my-portfolio2.js
+++ 100-projects-063-my-portfolio2.js
@@ -2,13 +2,13 @@
 
 //Define initial states for elements
 
-var contactList = document.getElementById('contact-list');
+const contactList = document.getElementById('contact-list');
 contactList.style.display = 'none';
 
-var notePopup = document.getElementById('note-popup');
+const notePopup = document.getElementById('note-popup');
 notePopup.style.display = 'none';
 
-var moreInfo = document.querySelector('.more-info');
+const moreInfo = document.querySelector('.more-info');
 moreInfo.style.display = 'none';
 
 document.getElementById('menu-icon').addEventListener('click', function (event) {
@@ -16,11 +16,11 @@ document.getElementById('menu-icon').addEventListener('click', function (event) 
     if (contactList.style.display === 'none') {
         contactList.style.display = 'block';
     } else {
-        notePopup.style.display = 'none';
+        contactList.style.display = 'none';
     }
 });
 
-document.getElementById('note-icon').addEventListener('clcik', function (event) {
+document.getElementById('note-icon').addEventListener('click', function (event) {
     event.preventDefault();
     if (notePopup.style.display === 'none') {
         notePopup.style.display = 'block';
@@ -30,13 +30,13 @@ document.getElementById('note-icon').addEventListener('clcik', function (event) 
 });
 
 document.getElementById('note-save').addEventListener('click', function() {
-    var noteText = document.getElementById('note-text').value;
+    const noteText = document.getElementById('note-text').value;
     localStorage.setItem('note', noteText);
     notePopup.style.display = 'none';
 });
 
 window.onload = function () {
-    var savedNote = localStorage.getItem('note');
+    const savedNote = localStorage.getItem('note');
     if (savedNote) {
         document.getElementById('note-text').value = savedNote;
     }
