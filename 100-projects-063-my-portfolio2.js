41d90399749824d7d6f5fcd17c1aa5c2a7f0ba0b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-24 11:15:22 -0500

    Added javascript functionalitly to page through added functions.

--- 100-projects-063-my-portfolio2.js
+++ 100-projects-063-my-portfolio2.js
@@ -16,7 +16,38 @@ document.getElementById('menu-icon').addEventListener('click', function (event) 
     if (contactList.style.display === 'none') {
         contactList.style.display = 'block';
     } else {
-        notePopup.style.display = 'noone';
+        notePopup.style.display = 'none';
+    }
+});
+
+document.getElementById('note-icon').addEventListener('clcik', function (event) {
+    event.preventDefault();
+    if (notePopup.style.display === 'none') {
+        notePopup.style.display = 'block';
+    } else {
+        notePopup.style.display = 'none';
+    }
+});
+
+document.getElementById('note-save').addEventListener('click', function() {
+    var noteText = document.getElementById('note-text').value;
+    localStorage.setItem('note', noteText);
+    notePopup.style.display = 'none';
+});
+
+window.onload = function () {
+    var savedNote = localStorage.getItem('note');
+    if (savedNote) {
+        document.getElementById('note-text').value = savedNote;
+    }
+};
+
+document.getElementById('btnView').addEventListener('click', function (event) {
+    event.preventDefault();
+    if (moreInfo.style.display === 'none') {
+        moreInfo.style.display = 'block';
+    } else {
+        moreInfo.style.display = 'none';
     }
 });
 
