23aa3f3eb12b46907db1bb4ceb428a24e8791fd7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-24 10:45:34 -0500

    initial variable states called and even listener for menu-icon creted.

--- 100-projects-063-my-portfolio2.js
+++ 100-projects-063-my-portfolio2.js
@@ -1,2 +1,22 @@
 'use strict'
 
+//Define initial states for elements
+
+var contactList = document.getElementById('contact-list');
+contactList.style.display = 'none';
+
+var notePopup = document.getElementById('note-popup');
+notePopup.style.display = 'none';
+
+var moreInfo = document.querySelector('.more-info');
+moreInfo.style.display = 'none';
+
+document.getElementById('menu-icon').addEventListener('click', function (event) {
+    event.preventDefault();
+    if (contactList.style.display === 'none') {
+        contactList.style.display = 'block';
+    } else {
+        notePopup.style.display = 'noone';
+    }
+});
+
