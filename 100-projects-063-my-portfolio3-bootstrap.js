432f9f23332f4beb86b66550bee6f80db9065d37
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-24 15:58:06 -0500

    Drafted functions for menu-icom, note-icon, note-save, window load, and btnView.

--- 100-projects-063-my-portfolio3-bootstrap.js
+++ 100-projects-063-my-portfolio3-bootstrap.js
@@ -1,1 +1,32 @@
 'use strict'
+
+//JQUERY for Interactivity
+
+$(document).ready(function() {
+    $("#menu-icon").click(function (e) {
+        e.preventDefault();
+        $("#contact-list").toggle();
+    });
+
+    $("#note-icon").click(function(e) {
+        e.preventDefault();
+        $("#note-popup").toggle();
+    });
+
+    $("#note-save").click(function(e) {
+        e.preventDefault();
+        $("#note-popup").hide();
+    });
+
+    $(window).on('load', function() {
+        var savedNote = localStorage.getItem('note');
+        if (savedNote) {
+            $("#note-text").val(savedNote);
+        }
+    });
+
+    $("#btnView").click(function(e) {
+        e.preventDefault();
+        $(".more-info").toggle();
+    });
+});
