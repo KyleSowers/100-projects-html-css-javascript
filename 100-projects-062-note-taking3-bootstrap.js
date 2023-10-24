469d7eea3b3e559e757f31c4913b2a722d940a49
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-23 21:09:32 -0500

    Same js fnctionality as previous two project examples, execpt this version was completed by altering to use of jquery.

--- 100-projects-062-note-taking3-bootstrap.js
+++ 100-projects-062-note-taking3-bootstrap.js
@@ -1,2 +1,34 @@
 'use strict'
 
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+$('#note-form').on('submit', function (e) {
+    e.preventDefault();
+
+    notes.push({title: $('#note-title').val(), text: $('#note-text').val()});
+    $('#note-title').val('');
+    $('#note-text').val('');
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+    displayNotes();
+});
+
+function displayNotes() {
+    const notesHTML = notes.map((note, i) => `
+    <div class="note card p-3 mb-3">
+    <h2 class="text-primary">${note.title}</h2>
+    <p>${note.text}</p>
+    <button class="btn btn-danger" onclick="deleteNote(${i})">Delete</button>
+    </div>
+    `).join('');
+
+    $('#notes').html(notesHTML);
+}
+
+function deleteNote(i) {
+    notes.splice(i, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    displayNotes();
+}
+
+displayNotes();
