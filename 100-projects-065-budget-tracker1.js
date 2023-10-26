fe0d17c5a93ae6ddce01debd3b5aaacd2c4d56f3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-26 08:30:27 -0500

    Set and stored js variables/values/events. Positive values add through income and negative values add through expenses with the addToIncomeList and addToExpensesList functions.

--- 100-projects-065-budget-tracker1.js
+++ 100-projects-065-budget-tracker1.js
@@ -1,2 +1,38 @@
 'use strict'
 
+let balance = 0;
+let income = [];
+let expenses = [];
+
+document.getElementById('money-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let desc = document.getElementById('desc').value;
+    let amount = Number(document.getElementById('amount').value);
+
+    if (amount >= 0) {
+        income.push({ desc: desc, amount: amount});
+        addToIncomeList(desc, amount);
+    } else {
+        expenses.push({desc: desc, amount: amount});
+        addToExpensesList(desc, amount);
+    }
+
+    balance += amount;
+    document.getElementById('balance').innerText = 'Balance: $' + balance;
+
+    document.getElementById('desc').value = '';
+    document.getElementById('amount').value = '';
+});
+
+function addToIncomeList(desc, amount) {
+    let listItem = document.createElement('li');
+    listItem.innerText = desc + ': $' + amount;
+    document.getElementById('income-list').appendChild(listItem);
+}
+
+function addToExpensesList(desc, amount) {
+    let listItem = document.createElement('li');
+    listItem.innerText = desc + ': $' + Math.abs(amount);
+    document.getElementById('expenses-list').appendChild(listItem);
+}
