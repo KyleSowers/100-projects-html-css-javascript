f796aaf5e801d3203cb53c1a4fee026abecf15a7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-25 20:59:53 -0500

    Added functionality by reformatting functions to use of jquery.

--- 100-projects-064-task-tracker3-bootstrap.js
+++ 100-projects-064-task-tracker3-bootstrap.js
@@ -1,2 +1,70 @@
 'use strict'
 
+$(document).ready(function() {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks = tasks.map(task => {
+        return {
+            ...task,
+            dueDate: new Date(task.dueDate),
+            completed: task.completed || false,
+        };
+    });
+
+    $('#task-form').on('submit', function(event) {
+        event.preventDefault();
+        const taskInput = $('#task-input');
+        const dueDateInput = $('#due-date-input');
+
+        const task = {
+            name: taskInput.val(),
+            dueDate: new Date(dueDateInput.val()),
+            complete: false,
+        };
+        tasks.push(task);
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        updateTaskDisplay();
+        taskInput.val('');
+        dueDateInput.val('');
+    });
+
+    $('#sort-asc').on('click', function () {
+        tasks.sort((a, b) => a.dueDate - b.dueDate);
+        updateTaskDisplay();
+    });
+
+    $('#sort-desc').on('click', function () {
+        tasks.sort((a, b) => b.dueDate - a.dueDate);
+        updateTaskDisplay();
+    });
+
+    function updateTaskDisplay() {
+        const taskList = $('#task-list tbody');
+        taskList.html('');
+
+        tasks.forEach((task, i) => {
+            const row = $('<tr>');
+            const taskCell = $('<td>').text(task.name).css('width', '220px');
+            const dateCell = $('<td>').text(isNaN(task.dueDate.getTime()) ? 'No Due Date' : task.dueDate.toDateString()).css('width', '120px');
+            const completeCell = $('<td>').css('width', '70px');
+            const completeCheckbox = $('<input>').attr('type', 'checkbox').prop('checked', task.completed).on('change', function () {
+                tasks[i].completed = this.checked;
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+                updateTaskDisplay();
+            });
+            completeCell.append(completeCheckbox);
+            const deleteCell = $('<td>').css('width', '80px');
+            const deleteButton = $('<button>').addClass('btn btn-danger').text('Delete').on('click', function() {
+                tasks.splice(i, 1);
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+                updateTaskDisplay();
+            });
+            deleteCell.append(deleteButton);
+            row.append(taskCell, dateCell, completeCell, deleteCell);
+            taskList.append(row);
+        });
+    }
+
+    updateTaskDisplay();
+});
