c75c415529abefb1b9694b0628622e6116f1110f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-26 21:44:54 -0500

    Added javascript functionality, additionally added edit functioning to both income and expenses.

--- 100-projects-065-budget-tracker2.js
+++ 100-projects-065-budget-tracker2.js
@@ -50,4 +50,83 @@ document.getElementById('expenses-form').addEventListener('submit', function (e)
 
 function addToIncomeList (desc, amount) {
     let listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <span>${desc}: $${amount.toLocaleString()}</span>
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</button>
+    `;
+    listItem.querySelector('.edit').addEventListener('click', function () {
+        let newDesc = prompt('Enter new description:', desc);
+        let newAmount = prompt('Enter new amount:', amount);
+        if (newDesc && newAmount) {
+            let index = income.findIndex(item => item.desc === desc && item.amount === amount);
+            income[index] = { desc: newDesc, amount: parseFloat(newAmount)};
+            localStorage.setItem('income', JSON.stringify(income));
+            listItem.querySelector('span').innerText = `${newDesc}: $${parseFloat(newAmount.toLocaleString())}`;
+            updateIncomeTotal();
+            updateExpensesTotal();
+            updateBalance();
+        }
+    });
+    listItem.querySelector('.delete').addEventListener('click', function () {
+        let index = income.findIndex(item => item.desc === desc && item.amount === amount);
+        income.splice(index, 1);
+        localStorage.setItem('income', JSON.stringify(income));
+        listItem.remove();
+        updateIncomeTotal();
+        updateExpensesTotal();
+        updateBalance();
+    });
+    document.getElementById('income-list').appendChild(listItem);
+}
+
+function  addToExpensesList(desc, amount) {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <span>${desc}: $${amount.toLocaleString()}</span>
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</button>
+    `;
+
+    listItem.querySelector('.edit').addEventListener('click', function () {
+        let newDesc = prompt('Enter new description');
+        let newAmount = prompt('Enter new amount', amount);
+        if (newDesc && newAmount) {
+            let index = expenses.findIndex(item => item.desc === desc && item.amount === amount);
+            expenses[index] = { desc: newDesc, amount: parseFloat(newAmount)};
+            localStorage.setItem('expenses', JSON.stringify(expenses));
+            listItem.querySelector('span').innerText = `
+            ${newDesc}: $${parseFloat(newAmount).toLocaleString()}`;
+            updateIncomeTotal();
+            updateExpensesTotal();
+            updateBalance();
+        }
+    });
+    listItem.querySelector('.delete').addEventListener('click', function () {
+        let index = expenses.findIndex(item => item.desc === desc && item.amount === amount);
+        expenses.splice(index, 1);
+        localStorage.setItem('expenses', JSON.stringify(expenses));
+        listItem.remove();
+        updateIncomeTotal();
+        updateExpensesTotal();
+        updateBalance();
+    });
+    document.getElementById('expenses-list').appendChild(listItem);
+}
+
+function updateIncomeTotal() {
+    let totalIncome = income.reduce((sum, item) => sum + item.amount, 0);
+    document.getElementById('income-total').innerText = 'Income Total: $' + totalIncome.toLocaleString();
+}
+
+function  updateExpensesTotal() {
+    let totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
+    document.getElementById('expenses-total').innerText = 'Expenses Total: $' + Math.abs(totalExpenses).toLocaleString();
+}
+
+function  updateBalance() {
+    let totalIncome = income.reduce((sum, item) => sum + item.amount, 0);
+    let totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
+    let balance = totalIncome - totalExpenses;
+    document.getElementById('balance').innerText = 'Balance: $' + balance.toLocaleString();
 }
