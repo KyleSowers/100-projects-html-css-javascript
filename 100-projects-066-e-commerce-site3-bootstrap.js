0601150472df48104abee981902229e1f671e614
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-28 11:13:48 -0500

    Added javascript functions with implimentation of jquery and additional bootstrap class styling for elements appended with jquery.

--- 100-projects-066-e-commerce-site3-bootstrap.js
+++ 100-projects-066-e-commerce-site3-bootstrap.js
@@ -11,3 +11,39 @@ let products = [
 
 let cart = [];
 
+$(document).ready(function () {
+//Display products
+    products.forEach(product => {
+        let productElement = $('<div>', { "class": 'product card m-2', "style": "width: 18rem;"}).appendTo('#products');
+        $('<img/>', { 'class': 'card-img-top', "src": product.image }).appendTo(productElement);
+        let cardBody = $('<div>', { 'class': 'card-body' }).appendTo(productElement);
+        $('<h5/>', { "class": 'card-title', "text": product.name }).appendTo(cardBody);
+        $('<p/>', { "class": 'card-text', "text": product.description }).appendTo(cardBody);
+        $('<p/>', { "class": 'card-text', "text": "Proce: $" + product.price }).appendTo(cardBody);
+        let quantityInput = $('<input/>', { "type": "number", "value": 1, "min": 1, "id": `quantity_${product.id}` }).appendTo(cardBody);
+        $('<button/>', { "class": 'btn btn-primary mt-2', "text": 'Add to Cart' }).click(function () {
+            addToCart(product, quantityInput.val());
+        }).appendTo(cardBody);
+    });
+});
+
+// Add to cart function
+function addToCart(produce, quantity) {
+    cart.push({ ...product, quantity });
+
+    let cartItemElement = $('<li/>', { "class": 'list-group-item', "text": product.name + ' (' + quantity + '): $' + product.price * quantity });
+    $('#cart').append(cartItemElement);
+}
+
+// Checkout function
+$('#checkout').click(function () {
+    if (cart.length > 0) {
+        let total = cart.reduce((total, product) => total + product.price * product.quantity, 0);
+        alert('Purchase made! Total cast: $' + total);
+        cart = [];
+        $("#cart").empty();
+    } else {
+        alert('Your cart is empty!');
+    }
+});
+
