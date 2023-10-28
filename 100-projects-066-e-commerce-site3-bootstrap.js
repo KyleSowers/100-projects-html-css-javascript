8190816b17d621f7496f662ffaf900bb3867bbeb
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-28 10:46:36 -0500

    created object array list of products.

--- 100-projects-066-e-commerce-site3-bootstrap.js
+++ 100-projects-066-e-commerce-site3-bootstrap.js
@@ -1,2 +1,13 @@
 'use strict'
 
+let products = [
+    { id: 1, name: 'Shoes 1', price: 100, category: 'Shoes', image: 'e-shoes1.png', description: 'Comfortable running shoes.' },
+    { id: 2, name: 'Hat 1', price: 50, category: 'Hats', image: 'e-hat1.png', description: 'Stylish baseball cap.' },
+    { id: 3, name: 'T-shirt', price: 20, category: "Clothes", image: 'e-tshirt1.jpg', description: 'Cotton t-shirt.' },
+    { id: 4, name: "Shoes 2", price: 100, category: "Shoes", image: 'e-shoes2.png', description: "Comfortable running shoes." },
+    { id: 5, name: "Hat 2", price: 50, category: "Hats", image: 'e-hat2.png', description: 'Stylish baseball cap.' },
+    { id: 6, name: "T-shirt", price: 20, category: "Clothes", image: 'e-tshirt2.jpg', description: 'Cotton t-shirt.' },
+];
+
+let cart = [];
+
