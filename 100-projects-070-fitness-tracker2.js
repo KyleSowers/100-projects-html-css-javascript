f84fc790c111771701d616dd4b1bb0cf3ec8042d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-31 18:30:10 -0500

    Variables retrieving workout values.

--- 100-projects-070-fitness-tracker2.js
+++ 100-projects-070-fitness-tracker2.js
@@ -1,2 +1,13 @@
 'use strict'
 
+document.getElementById('workout-form').addEventListener('submit', function (event) {
+//     Prevent the form from submitting
+    event.preventDefault();
+
+//     Get the workout data
+    var date = document.getElementById('date').value;
+    var workoutType = document.getElementById('workout-type').value;
+    var duration = document.getElementById('duration').value;
+
+//
+})
