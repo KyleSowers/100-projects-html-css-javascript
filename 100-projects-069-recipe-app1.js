4d9f6eb81dad3adc6e696c37e2c13159ceb6bece
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-30 19:15:12 -0500

    created handleSubmit function.

--- 100-projects-069-recipe-app1.js
+++ 100-projects-069-recipe-app1.js
@@ -1,2 +1,24 @@
 'use strict'
 
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#recipe-name');
+const ingrInput = document.querySelector('#recipe-ingredients');
+const methodInput = document.querySelector('#recipe-method');
+const rightColumn = document.querySelector('.right-column');
+let recipes = [];
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = nameInput.value.trim();
+    const ingredients = ingrInput.value.trim().split(',').map(i => i.trim());
+    const method = methodInput.value.trim();
+
+    if (name && ingredients.length > 0 && method) {
+        const newRecipe = { name, ingredients, method };
+        recipes.push(newRecipe);
+        nameInput.value = '';
+        ingrInput.value = '';
+        methodInput.value = '';
+        displayRecipes();
+    }
+}
