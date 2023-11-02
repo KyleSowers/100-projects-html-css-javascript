fa204f757146fa569967796467f44225f4b99713
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-01 20:38:39 -0500

    Added variables for audio/play/pause/skip/replay.

--- 100-projects-071-music-player1.js
+++ 100-projects-071-music-player1.js
@@ -1,2 +1,11 @@
 'use strict'
 
+window.onload = function () {
+    var audio = document.getElementById('audio');
+    var playButton = document.getElementById('play');
+    var pauseButton = document.getElementById('pause');
+    var skipButton = document.getElementById('skip');
+    var replayButton = document.getElementById('replay');
+
+
+}
