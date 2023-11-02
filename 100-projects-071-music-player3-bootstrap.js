2f0ee73555cb102dc71031fe64019c5a5de2d65e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-02 08:53:22 -0500

    Called needed ids from html into variables in javascript for use, by means of jquery.

--- 100-projects-071-music-player3-bootstrap.js
+++ 100-projects-071-music-player3-bootstrap.js
@@ -1,2 +1,14 @@
 'use strict'
 
+$(document).ready(function () {
+    var audio = $('#audio')[0];
+    var source = $('#source');
+    var playButton = $('#play');
+    var nextButton = $('#next');
+    var prevButton =$('#prev');
+    var replayButton = $('#replay');
+    var volumeControl = $('#volume');
+    var currentTimeDisplay = $('#currentTime');
+    var songTitle = $('#songTitle');
+
+})
