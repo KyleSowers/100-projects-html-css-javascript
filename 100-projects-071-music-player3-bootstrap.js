1dae5da8a5c4f196628431887ad358b92db5d2bd
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-02 11:06:35 -0500

    Prepared html/css/js files for 100-projects-072-quiz-app1 project.

--- 100-projects-071-music-player3-bootstrap.js
+++ 100-projects-071-music-player3-bootstrap.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     var playButton = $('#play');
     var nextButton = $('#next');
     var prevButton =$('#prev');
+    var pauseButton = $('#pause');
     var replayButton = $('#replay');
     var volumeControl = $('#volume');
     var currentTimeDisplay = $('#currentTime');
@@ -24,6 +25,37 @@ $(document).ready(function () {
         audio.play();
     })
 
+    nextButton.on('click', function () {
+        currentSongIndex = (currentSongIndex + 1) % songs.length;
+        loadSong();
+        audio.play();
+    });
 
+    prevButton.on('click', function () {
+        currentSongIndex = (currentSongIndex - 1) % songs.length;
+        loadSong();
+        audio.play();
+    });
 
-})
+    pauseButton.on('click', function () {
+        audio.pause();
+    });
+
+    replayButton.on('click', function () {
+        audio.currentTime = 0;
+        audio.play();
+    });
+
+    volumeControl.on('input', function () {
+        audio.volume = this.value;
+    });
+
+    audio.ontimeupdate = function () {
+        var minutes = Math.floor(audio.currentTime / 60);
+        var seconds = Math.floor(audio.currentTime % 60);
+        currentTimeDisplay.text(minutes + ':' + (seconds < 10 ? '0' + seconds : seconds));
+    }
+
+    loadSong();
+
+});
