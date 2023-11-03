f85bc89591e2394c84b4ddb6bd131685127fc2a4
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-03 08:39:17 -0500

    Added event listener to get title and author values stored into variables for use.

--- 100-projects-073-library-management1.js
+++ 100-projects-073-library-management1.js
@@ -1,2 +1,12 @@
 'use strict'
 
+let library = [];
+
+document.getElementById("addBookForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    addBookToLibrary(title, author);
+    displayBooks();
+});
+
