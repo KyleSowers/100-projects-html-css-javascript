445d00fb03b389919ce183685565d6589c2a3339
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-03 09:13:27 -0500

    Created the displaBooks function.

--- 100-projects-073-library-management1.js
+++ 100-projects-073-library-management1.js
@@ -14,3 +14,18 @@ function addBookToLibrary(title, author) {
     library.push({title: title, author: author, status: 'Available'});
 }
 
+function displayBooks() {
+    const bookList = document.getElementById('booklist');
+    bookList.innerHTML = '';
+    for (let i = 0; i < library.length; i++) {
+        const book = document.getElementById('bookList');
+        bookList.innerHTML = '';
+        for (let i = 0; i < library.length; i++) {
+            const book = document.getElementById('bookList');
+            book.innerHTML = `<h2>${library[i].title}</h2> <h3>${library[i].author}</h3> <p>${library[i].status}</p> <button onclick="loanBook(${i})">Loan Book</button> <button onclick="returnBook(${i})">Return Book</button> <hr>`;
+                bookList.append(book);
+        }
+    }
+
+
+}
