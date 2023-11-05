bac66486eb4979b250b11428ba2053f01108ff52
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-04 21:26:41 -0500

    Added javascript mymap variable, tile layer and marker.

--- 100-projects-074-interactive-map1.js
+++ 100-projects-074-interactive-map1.js
@@ -1,2 +1,11 @@
 'use strict'
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+     maxZoom: 19,
+     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
