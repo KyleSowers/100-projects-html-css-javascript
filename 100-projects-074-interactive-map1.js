c7cab0289af3caa44e6b025ec32d9e165ff830e9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-04 21:46:11 -0500

    Prepared html/js files with imported unpkg.com link and script.

--- 100-projects-074-interactive-map1.js
+++ 100-projects-074-interactive-map1.js
@@ -5,7 +5,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
      maxZoom: 19,
-     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
