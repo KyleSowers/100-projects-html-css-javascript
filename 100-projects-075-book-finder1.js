2858fc1086b945b74951d7c7a419896c34798983
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-05 17:47:17 -0600

    added javascript fetchbooks function and getBooks function.

--- 100-projects-075-book-finder1.js
+++ 100-projects-075-book-finder1.js
@@ -1,2 +1,39 @@
 'use strict'
 
+document.getElementById('book-form').addEventListener('submit', fetchbooks);
+
+function fetchBooks(e) {
+    e.preventDefault();
+
+//     Get the search term
+    let searchTerm = document.getElementById('search').value;
+
+//     Call the Goole Books API
+    getBooks(searchTerm);
+}
+
+function getBooks(searchTerm) {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let output = '<h2>Book Results</h2>';
+            data.items.forEach(book => {
+                let title = book.volumeInfo.title;
+                let authors = book.volumeInfo.authors;
+                let description = book.volumeInfo.description;
+                let imgLink = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumnail : "Image Not Available";
+
+                output += `
+                <div>
+                    <img src="${imgLink}" alt="book-image">
+                    <h3>${title}</h3>
+                    <h4>${authors}</h4>
+                    <p>${description}</p>
+                </div><br>`;
+            });
+            document.getElementById('result').innerHTML = output;
+        })
+        .catch(err => console.log(err));
+}
