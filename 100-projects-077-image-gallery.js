a5babab23e228a7c27ebc822c810eed7a9e6fd34
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-07 19:16:08 -0600

    set forEach on gallery images for display.

--- 100-projects-077-image-gallery.js
+++ 100-projects-077-image-gallery.js
@@ -5,3 +5,11 @@ const modalImg = document.getElementById("img01");
 const captionText = document.getElementById("caption");
 const galleryImages = document.querySelectorAll(".galleryImage");
 const close = document.getElementsByClassName('close')[0];
+
+galleryImages.forEach(img => {
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
