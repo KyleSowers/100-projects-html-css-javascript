6936d81a6867ccb25f4bf0167998c4e56ccc5e6a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-08 19:08:01 -0600

    Added object array of menu items.

--- 100-projects-078-menu-manager1.js
+++ 100-projects-078-menu-manager1.js
@@ -1,2 +1,8 @@
 'use strict'
 
+let menu = [
+    { name: 'Burger', category: 'main course', price: '$10'},
+    { name: 'Salad', category: "starter", price: '$5'},
+    { name: "Ice Cream", category: 'dessert', price: "$3"},
+];
+
