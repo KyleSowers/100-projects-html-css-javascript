d287990f7ed55a22aae1a86ceaab8f86a4a7572a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-08 19:38:05 -0600

    Added object array of menu items.

--- 100-projects-078-menu-manager2-bootstrap.js
+++ 100-projects-078-menu-manager2-bootstrap.js
@@ -1,2 +1,7 @@
 'use strict'
 
+let menu = [
+    { name: 'Burger', category: 'main course', price: '$10', image: "menu_burger.jpg"},
+    { name: 'Salad', category: "starter", price: '$5', image: "menu_salad.jpg"},
+    { name: "Ice Cream", category: 'dessert', price: "$3", image: "menu_icecream.jpg"},
+]
