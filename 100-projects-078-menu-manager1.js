e020a8e621b4f1dcbc630e3da6b50540d09613f8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-08 19:13:12 -0600

    Added renderMenu function.

--- 100-projects-078-menu-manager1.js
+++ 100-projects-078-menu-manager1.js
@@ -6,3 +6,19 @@ let menu = [
     { name: "Ice Cream", category: 'dessert', price: "$3"},
 ];
 
+function renderMenu() {
+    const menuContainer = document.getElementById('menu');
+    menuContainer.innerHTML = '';
+    menu.forEach((item, index) => {
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('menuItem');
+        itemElement.innerHTML = `
+        <h2>${item.name}</h2>
+        <p>Category: ${item.category}</p>
+        <p>Price: ${item.price}</p>
+        <button onclick="removeItem(${index})">Remove</button>
+        `;
+        menuContainer.appendChild(itemElement);
+    });
+}
+
