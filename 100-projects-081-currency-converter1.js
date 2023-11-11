ecef1209e0e89a701406d465b56c5816b3ec341c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-11 11:44:26 -0600

    Added fetch call to api for currency exchange results.

--- 100-projects-081-currency-converter1.js
+++ 100-projects-081-currency-converter1.js
@@ -5,3 +5,18 @@ const toCurrentSelect = document.getElementById('to-currency');
 const amountInput = document.getElementById('amount');
 const resultParagraph = document.getElementById('result');
 
+let exchangeRates;
+
+//Fetch the latest exchange rates
+fetch('https://v6.exchangerate-api.com/v6/aaf53da1eba53d98819909**/latest/USD')
+    .then(response => response.json())
+    .then(data => {
+        exchangeRates = data.conversion_rates;
+
+    //     Populate the select elements with the currency options
+        Object.keys(exchangeRates).forEach(currency => {
+            fromCurrencySelect.innerHTML += ` <option value="${currency}">${currency}</option> `;
+            toCurrentSelect.innerHTML += ` <option value="${currency}">${currency}</option> `;
+        });
+    });
+
