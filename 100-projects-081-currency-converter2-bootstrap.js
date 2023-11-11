a6900d8edf551e7746edfef1e61361eeee93997b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-11 12:20:17 -0600

    Added needed ID calls for values stroes in variables, for javascript usage.

--- 100-projects-081-currency-converter2-bootstrap.js
+++ 100-projects-081-currency-converter2-bootstrap.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const fromCurrencySelect = document.getElementById('from-currency');
+const toCurrencySelect = document.getElementById('to-currency');
+const amountInput = document.getElementById('amount');
+const resultParagraph = document.getElementById('result');
+
+let exchangeRates;
+
