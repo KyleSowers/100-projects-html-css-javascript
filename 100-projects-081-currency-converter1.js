702e46c1aa467ffd6ca5e32d7295be9b47698fd2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-11 11:36:47 -0600

    Added const variables needed for javascript functionality.

--- 100-projects-081-currency-converter1.js
+++ 100-projects-081-currency-converter1.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const fromCurrencySelect = document.getElementById('from-currency');
+const toCurrentSelect = document.getElementById('to-currency');
+const amountInput = document.getElementById('amount');
+const resultParagraph = document.getElementById('result');
+
