1e1073a911133cc16de317cc6288a26c841dad45
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-11 12:03:10 -0600

    Tested with personal APIKEY from exchangerate-api.com. Personal key removed and replaced with YOURAPIKEYHERE.

--- 100-projects-081-currency-converter1.js
+++ 100-projects-081-currency-converter1.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const fromCurrencySelect = document.getElementById('from-currency');
-const toCurrentSelect = document.getElementById('to-currency');
+const toCurrencySelect = document.getElementById('to-currency');
 const amountInput = document.getElementById('amount');
 const resultParagraph = document.getElementById('result');
 
 let exchangeRates;
 
 //Fetch the latest exchange rates
-fetch('https://v6.exchangerate-api.com/v6/aaf53da1eba53d98819909**/latest/USD')
+fetch('https://v6.exchangerate-api.com/v6/YOURAPIKEYHERE/latest/USD')
     .then(response => response.json())
     .then(data => {
         exchangeRates = data.conversion_rates;
@@ -16,13 +16,13 @@ fetch('https://v6.exchangerate-api.com/v6/aaf53da1eba53d98819909**/latest/USD')
     //     Populate the select elements with the currency options
         Object.keys(exchangeRates).forEach(currency => {
             fromCurrencySelect.innerHTML += ` <option value="${currency}">${currency}</option> `;
-            toCurrentSelect.innerHTML += ` <option value="${currency}">${currency}</option> `;
+            toCurrencySelect.innerHTML += ` <option value="${currency}">${currency}</option> `;
         });
     });
 
 function convertCurrency() {
     const fromCurrency = fromCurrencySelect.value;
-    const toCurrency = toCurrentSelect.value;
+    const toCurrency = toCurrencySelect.value;
     const amount = amountInput.value;
 
     const result = amount / exchangeRates[fromCurrency] * exchangeRates[toCurrency];
