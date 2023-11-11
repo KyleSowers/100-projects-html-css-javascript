164f71330589bd5f53851b72e625d7bc6d92da06
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-10 20:02:32 -0600

    Added javascript functionalities as previous version but with use of jquery.

--- 100-projects-080-markdown-previewer3-bootstrap.js
+++ 100-projects-080-markdown-previewer3-bootstrap.js
@@ -1,2 +1,42 @@
 'use strict'
 
+$(document).ready(function () {
+    function updatePreview() {
+        const markdown = $('#editor').val();
+        $('#preview').html(marked(markdown));
+    }
+
+    $('#editor').on('input', updatePreview);
+
+    $('#loadSample').on('click', function () {
+        const sampleMarkdown = `
+        # Hello World!
+        
+        This is a sample markdown.
+        
+        ## This is a sub heading
+        
+        **This text will be bold**
+        
+        _This text will be italic_
+        
+        - This is a list item
+        
+        [This is a link](http://www.example.com)
+        
+        \`\`\`
+        This is a code block
+        \`\`\`
+        
+        > This is a quote
+        `;
+
+        $('#editor').val(sampleMarkdown);
+        updatePreview();
+    });
+
+    $(#clear).on('click', function () {
+        $('#editor').val('');
+        updatePreview();
+    });
+});
