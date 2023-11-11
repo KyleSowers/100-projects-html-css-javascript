f70a65465513fb0da886fa086db7066f14cb5807
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-10 19:31:48 -0600

    Added same variables as previous projects and function to update preview. This version added event listeners and functions for loadingSample and clearing.

--- 100-projects-080-markdown-previewer2.js
+++ 100-projects-080-markdown-previewer2.js
@@ -1,2 +1,47 @@
 'use strict'
 
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
+
+editor.addEventListener('input', updatePreview);
+
+function updatePreview() {
+    const markdown = editor.value;
+    preview.innerHTML = marked(markdown);
+}
+
+document.getElementById('loadSample').addEventListener('click', loadSampleMarkdown);
+document.getElementById('clear').addEventListener('click', clearMarkdown);
+
+function loadSampleMarkdown() {
+    const sampleMarkdown = `
+    # Hello World!
+    
+    This is a sample markdown.
+    
+    ## This is a sub-heading
+    
+    **This text will be bold**
+    
+    _This text will be italic_
+    
+    - This is a list item
+    
+    [This is a link](http://www.example.com)
+    
+    \`\`\`
+    This is a code block
+    \`\`\`
+    
+    > This is a quote
+    `;
+    ;
+    editor.value = sampleMarkdown;
+    updatePreview();
+}
+
+function clearMarkdown() {
+    editor.value = '';
+    updatePreview();
+}
+
