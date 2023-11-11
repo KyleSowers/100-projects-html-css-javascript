a74587e3ddaa91a48c94e53808b85e0636757a48
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-10 18:43:12 -0600

    Added variables and a simple function updatePreview that takes the value and adds to preview.

--- 100-projects-080-markdown-previewer1.js
+++ 100-projects-080-markdown-previewer1.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
+
+editor.addEventListener('input', updatePreview);
+
+function updatePreview() {
+    const markdown = editor.value;
+    preview.innerHTML = marked(markdown);
+}
+
