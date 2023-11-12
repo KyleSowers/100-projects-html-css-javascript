3498c516bad2d2e99545b4284bd8ff1a0b80f4e8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-12 17:58:38 -0600

    Added fetch for api of a job board which returns desired results and will display through the use of createlement and inner html.

--- 100-projects-082-job-board1.js
+++ 100-projects-082-job-board1.js
@@ -1,2 +1,14 @@
 'use strict'
 
+fetch('https://www.themuse.com/api/public/jobs?page=1')
+    .then(response => response.json())
+    .then(data => {
+        let jobList = document.getElementById('job-list');
+        data.results.forEach(job => {
+            let jobDiv = document.createElement('div');
+            jobDiv.classList.add('job');
+            jobDiv.innerHTML = ` <h2>${job.name}</h2><a href="${job.refs.landing_page}" target="_blank">Apply Now</a>`;
+            jobList.appendChild(jobDiv);
+        });
+    })
+    .catch(error => console.error('Error:', error));
