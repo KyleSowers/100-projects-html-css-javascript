96f142af77f705146e2acda1d28e272c5a08e5c1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-12 18:20:02 -0600

    Added fetch api call.

--- 100-projects-082-job-board2.js
+++ 100-projects-082-job-board2.js
@@ -1,2 +1,14 @@
 'use strict'
 
+fetch('https://my-json-server.typicode.com/SenghyunNa-web/jobboard/jobs')
+    .then(response => response.json())
+    .then(data => {
+        let jobList = document.getElementById('job-list');
+        data.forEach(job => {
+            let jobDiv = document.createElement('div');
+            jobDiv.classList.add('job');
+            jobDiv.innerHTML = ` <h2>${job.title}</h2><p>${job.location}</p><a href="${job.url}" target="_blank">Apply Now</a>`;
+            jobList.appendChild(jobDiv);
+        });
+    })
+    .catch(error => console.error('Error:', error));
