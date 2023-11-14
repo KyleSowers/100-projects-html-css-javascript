e4c59cc944818e7c0e4414b0405fb50a3c23ba20
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-13 18:10:46 -0600

    Added javascript functions of add/delete/render trips.

--- 100-projects-083-travel-planner1.js
+++ 100-projects-083-travel-planner1.js
@@ -1,2 +1,31 @@
 'use strict'
 
+const trips = [];
+
+function addTrip() {
+    const destination = document.getElementById("destination").value;
+    const date = document.getElementById("date").value;
+
+    if (destination && date) {
+        const trip = ( destination, date );
+        trips.push(trip);
+        document.getElementById('destination').value = '';
+        document.getElementById('date').value = '';
+        renderTrips();
+    } else {
+        alert('Please enter a destination and daate');
+    }
+}
+
+function deleteTrip(index) {
+    trips.splice(index, 1);
+    renderTrips();
+}
+
+function renderTrips() {
+    const tripsDiv = document.getElementById('trips');
+    tripsDiv.innerHTML = '';
+    trups.forEach((trip, index) => {
+        tripsDiv.innerHTML += ` <div class="trip"><span>${trip.destination}</span><button onclick="deleteTrip(${index})">Delete</button>`
+    });
+}
