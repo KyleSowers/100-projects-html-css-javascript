1b68ae2acb63d16d872ebf0cd4a86bfbdbd9db2c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-14 19:47:26 -0600

    Added functions feedPet, playWithPet, napPet, updateStatus and setInterval.'

--- 100-projects-084-virtual-pet-game3-bootstrap.js
+++ 100-projects-084-virtual-pet-game3-bootstrap.js
@@ -1,2 +1,41 @@
 'use strict'
 
+let petHunger = 0
+let petBoredom = 0
+const statusDiv = document.getElementById('status');
+const petDiv = document.getElementById('pet');
+
+function feedPet() {
+    petHunger -= 10;
+    if (petHunger < 0) petHunger = 0;
+    updateStatus();
+}
+
+function playWithPet() {
+    petBoredom -= 10;
+    if (petBoredom < 0) petBoredom = 0;
+    updateStatus();
+}
+
+function napPet() {
+    petBoredom -= 15;
+    if (petBoredom < 0) petBoredom = 0;
+    updateStatus();
+}
+
+function updateStatus() {
+    statusDiv.textContent = `Pet hunger: ${petHunger}, Pet boredom: ${petBoredom}`;
+    if (petHunger > 20) {
+        petDiv.textContent = "🐶💭🍖🥩";
+    } else if (petBoredom > 20) {
+        petDiv.textContent = "🐶💭⚽️🏃⚾️";
+    } else {
+        petDiv.textContent = "🐶";
+    }
+}
+
+setInterval(() => {
+    petHunger += 1;
+    petBoredom += 1;
+    updateStatus();
+}, 1000);
