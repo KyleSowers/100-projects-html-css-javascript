f14c5b6fbbafb17a3cdb76d6e66ea95ce722ecf8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-14 19:21:58 -0600

    Added functions feedPet, playWithPet, UpdateStatus, and setInterval.

--- 100-projects-084-virtual-pet-game2.js
+++ 100-projects-084-virtual-pet-game2.js
@@ -1,2 +1,35 @@
 'use strict'
 
+let petHunger = 0;
+let petBoredom = 0;
+const statusDiv = document.getElementById('status');
+const petDiv = document.getElementById('pet');
+
+function feedPet() {
+    petHunger -= 10;
+    if (petHunger < 0) petHunger = 0;
+    updateStatus();
+}
+
+function playWithPet() {
+    petBoredom -= 10;
+    if (petBoredom < 0) petBoredom = 0;
+    updateStatus();
+}
+
+function updateStatus() {
+    statusDiv.textContent = `Pet hunger: ${petHunger}, Pet boredom: ${petBoredom}`;
+    if (petHunger > 10) {
+        petDiv.textContent = "🐶💭🍖🥩"; //Hungry dod
+    } else if (petBoredom > 20) {
+        petDiv.textContent = "🐶💭⚽️🏃⚾️"; //bored dog
+    } else {
+        petDiv.textContent = "🐶"; //Happy dog
+    }
+}
+
+setInterval(() => {
+    petHunger += 1;
+    petBoredom += 1;
+    updateStatus();
+}, 1000);
