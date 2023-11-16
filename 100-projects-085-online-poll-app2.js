32e5a2d34f087bc0f2919dd9b9046373a34f24a9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-15 19:29:53 -0600

    Added variable of object array (dog & cat). Function vote for increasing count of poll selections. Function displayResults, but with the creation of an increasing bar for either poll count that spans the page but divides between count percentages.

--- 100-projects-085-online-poll-app2.js
+++ 100-projects-085-online-poll-app2.js
@@ -1,2 +1,23 @@
 'use strict'
 
+let pollResults = {
+    Dog: 0,
+    Cat: 0
+};
+
+function vote(option) {
+    pollResults[option]++;
+    displayResults();
+}
+
+function displayResults() {
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '';
+    for (let option in pollResults) {
+        const bar = document.createElement('div');
+        bar.className = 'bar';
+        bar.style.width = (pollResults[option] * 4) + '%';
+        bar.textContent = `${option}: ${pollResults[option]}`;
+        resultsDiv.appendChild(bar);
+    }
+}
