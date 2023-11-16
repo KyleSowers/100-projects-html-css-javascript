619930424d97659880afc66bbfce790eed417b76
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-15 19:10:42 -0600

    Added variable with stored objects array of dog and cat. MAde function vote which added increase of count per selected option. Added function display results, to display the results in the results div.

--- 100-projects-085-online-poll-app1.js
+++ 100-projects-085-online-poll-app1.js
@@ -1,2 +1,19 @@
 'use strict'
 
+let pollResults = {
+    Dog: 0,
+    Cat: 0
+};
+
+function vote(option) {
+    pollResults[option]++;
+    displayResults();
+}
+
+function displayResults() {
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = "";
+    for (let option in pollResults) {
+        resultsDiv.innerHTML += ` <p>${option}: ${pollResults[option]}</p>`;
+    }
+}
