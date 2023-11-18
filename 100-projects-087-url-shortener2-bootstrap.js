b4cea9c7e4b67212581b46eceeab120f074e48d7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-17 21:26:47 -0600

    Added function shortenUrl which calls upon api to return result. Then additionaliy created a copyToClipboard function to save results for user to a clipboard.

--- 100-projects-087-url-shortener2-bootstrap.js
+++ 100-projects-087-url-shortener2-bootstrap.js
@@ -1,2 +1,56 @@
 'use strict'
 
+$(document).ready(function () {
+    $('shorten-btn').click(function () {
+        shortenUrl();
+    });
+});
+
+function shortenUrl() {
+    const urlInput = $('#url-input').val();
+    fetch('https://api.rebrandly.com/v1/links', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'aplication/json',
+            'apikey': 'YOURAPIKEYHERE',
+        },
+        body: JSON.stringify({
+            destination: urlInput
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data.shortUrl) {
+                $('result').html(`
+                <div class="alert alert-success">
+                Shortened URL: <a href="https://${data.shortUrl}" target="_blank">${data.shortUrl}</a>
+                <button class="btn btn-primary ml-2" onclick="copyToClipboard('${data.shortURL}')">Copy to Clipboard</button>
+                </div>
+                `);
+            } else {
+                $('#result').html(`
+                <div class="alert alert-danger">
+                Failed to shorten the URL. PLease try again.
+                </div>
+                `);
+             }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function copytoClipboard(text) {
+    const input = document.createElement('input');
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+
+    $('#result').append('<div class="alert alert alert=success mt-2">URL copied to clipboard!</div>');
+    setTimeout(() => {
+        $('.alert-success').remove();
+    }, 2000);
+}
+
