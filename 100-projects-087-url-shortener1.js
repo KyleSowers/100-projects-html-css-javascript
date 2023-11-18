0b78c17857857727ba6b9fcfa6b10df6e94e7bc1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-17 18:20:46 -0600

    added the shortenUrl function which contains an API call to rebrandly which will shorten and display the shortened url as a clickable link that opens a new tab.

--- 100-projects-087-url-shortener1.js
+++ 100-projects-087-url-shortener1.js
@@ -1,2 +1,19 @@
 'use strict'
 
+function shortenUrl() {
+    const urlInput = document.getElementById('url-input').value;
+
+    fetch('https://api.rebrandly.com/v1/links', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey': 'YOURAPIKEYHERE',},
+        body: JSON.stringify({ destination: urlInput})
+        })
+        .then(response => response.json())
+        .then(data => {document.getElementById('result').innerHTML = ` <a href="https://${data.shortUrl}</a>`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
