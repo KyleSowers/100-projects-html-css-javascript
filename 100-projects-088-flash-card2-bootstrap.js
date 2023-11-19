82e1dfc3411255b5017b45e62d58497b6e40a0f4
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-18 21:58:27 -0600

    Added shufflecards() function.

--- 100-projects-088-flash-card2-bootstrap.js
+++ 100-projects-088-flash-card2-bootstrap.js
@@ -3,3 +3,12 @@
 function flipCard(card) {
     card.classList.toggle('card-flipped');
 }
+
+function shuffleCards() {
+    const flashcards = document.querySelectorAll('.flashcard-grid');
+    const shuffledCards = Array.from(flashcards).sort(() => Math.random() - 0.5);
+    shuffledCards.foreach((card, index) => {
+        card.style.order = index;
+        card.classList.remove('card-flipped');
+    });
+}
