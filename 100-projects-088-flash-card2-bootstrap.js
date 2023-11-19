44ad25624175214f1a1ab4c19db593e6c0a90c42
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-18 22:04:19 -0600

    Added a resetCard() function.

--- 100-projects-088-flash-card2-bootstrap.js
+++ 100-projects-088-flash-card2-bootstrap.js
@@ -5,10 +5,17 @@ function flipCard(card) {
 }
 
 function shuffleCards() {
-    const flashcards = document.querySelectorAll('.flashcard-grid');
+    const flashcards = document.querySelectorAll('.flashcard-grid .card');
     const shuffledCards = Array.from(flashcards).sort(() => Math.random() - 0.5);
-    shuffledCards.foreach((card, index) => {
+    shuffledCards.forEach((card, index) => {
         card.style.order = index;
+        card.classList.remove('card-flipped');
+    });
+
+}
+function resetCards() {
+    const flashcards = document.querySelectorAll('.flashcard-grid .card');
+    flashcards.forEach(card => {
         card.classList.remove('card-flipped');
     });
 }
