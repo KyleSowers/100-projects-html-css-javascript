ceb98d46dcb9d0af00a10877269bf4d44a81b4c5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-19 20:02:32 -0600

    Added api call.

--- 100-projects-089-geocoding-app2-bootstrap.js
+++ 100-projects-089-geocoding-app2-bootstrap.js
@@ -1,2 +1,41 @@
 'use strict'
 
+$(document).ready(function () {
+    //Event listener for the form submission
+    $('#geocode-form').submit(function (e) {
+        e.preventDefault();
+
+        var location = $('#location-input').val();
+
+        axios.get('https://api.opencagedata.com/geocode/v1/json', {
+            params: {
+                q: location,
+                key: 'YOURAPIKEYHERE'
+            }
+        })
+            .then(function (response) {
+                var formattedAddress = response.data.results[0].formatted;
+                var lat = response.data.results[0].geometry.lat;
+                var lng = response.data.results[0].geometry.lng;
+
+                var formattedAddressOutput = `
+                <ul class='list-group'>
+                    <li class="list-group-item">${formattedAddress}</li>
+                </ul>
+                `;
+
+                var geometryOutput = `
+                <ul class='list-group'>
+                    <li class="list-group-item"><strong>Latitudw: </strong>${lat}</li>
+                    <li class="list-group-item"><strong>Longitutde: </strong>${lng}</li>
+                </ul>
+                `;
+
+                $('#result').html(formattedAddressOutput + geometryOutput);
+            })
+            .catch(function (error) {
+            console.log(error);
+    });
+});
+
+
