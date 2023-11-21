727a86fe2fcc0f2de94ab8ff9b1b2e831498f815
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-21 14:25:29 -0600

    Created a fetch call to API_URL.

--- 100-projects-091-product-inventory1.js
+++ 100-projects-091-product-inventory1.js
@@ -1,2 +1,19 @@
 'use strict'
 
+const form = document.getElementById('product-form');
+const nameInout = document.getElementById('productName');
+const descriptionInput = document.getElementById('productDescription');
+const priceInput = document.getElementById('productPrice');
+const quantityInput = document.getElementById('productQuantity');
+const productList = document.getElementById('product-list');
+const API_URL = 'https://my-json-server.tyicode.com/SanghyunNa-web/inventory/products';
+
+//Fetch initial products
+fetch(API_URL)
+.then((response) => response.json())
+.then((products) => {
+    products.forEach((product) => {
+        addProductToPage(product);
+    });
+});
+
