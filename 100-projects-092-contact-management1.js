6c00fd6b23d0b9cc6e6c8d454b6daca32605be19
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-22 10:21:20 -0600

    testing

--- 100-projects-092-contact-management1.js
+++ 100-projects-092-contact-management1.js
@@ -1,2 +1,11 @@
 'use strict'
 
+const form = document.getElementById('contact-form');
+const nameInput = document.getElementById('contactName');
+const emailInput = document.getElementById('contactEmail');
+const phoneInput = document.getElementById('contactPhone');
+const addressInput = document.getElementById('contactAddress');
+const contactList = document.getElementById('contact-list');
+const API_URL = 'https://my-json-server.typicode.com/SanghyunNa-web/contactmanage/contacts';
+
+
