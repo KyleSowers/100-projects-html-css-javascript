c8022fded53aa12b5aeb13262f5e16529aec2c62
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-22 12:00:02 -0600

    Added variables calling ids from html.

--- 100-projects-092-contact-management2-bootstrap.js
+++ 100-projects-092-contact-management2-bootstrap.js
@@ -1,2 +1,10 @@
 'use strict'
 
+const form = document.getElementById('contact-form');
+let nameInput = document.getElementById('contactName');
+let emailInput = document.getElementById('contactEmail');
+let phoneInput = document.getElementById('contactPhone');
+let addressInput = document.getElementById('contactAddress');
+const contactList = document.getElementById('contact-list');
+const searchInput = document.getElementById('searchInput');
+const API_URL = 'https://my-json-server.typicode.com/SanghyunNa-web/contactmanage/contacts';
