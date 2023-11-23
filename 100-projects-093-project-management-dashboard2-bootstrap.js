0942e274ed2729c9bcee06cb0d7a4d2bfa2ff8b9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-23 15:44:38 -0600

    Called on project-list and project-form from html to store into variables. Then created the locally stored projects object array with two premade projects.

--- 100-projects-093-project-management-dashboard2-bootstrap.js
+++ 100-projects-093-project-management-dashboard2-bootstrap.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const projectList = document.getElementById("project-list");
+const projectForm = document.getElementById("project-form");
+
+//Local data
+const projects = [
+    {
+        name: "Project 1",
+        description: "This is project 1",
+        status: "In Progress",
+        teamMembers: ["Alice", "Bob"]
+    },
+    {
+        name: "Project 2",
+        description: "This is project 2",
+        status: "Completed",
+        teamMembers: ["Charlie", "Dave"]
+    },
+    //Can add as many as desired
+];
