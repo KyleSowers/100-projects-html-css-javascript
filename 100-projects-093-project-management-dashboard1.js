bff672692958b113536bbb038cf5b73d716f7758
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-23 15:06:23 -0600

    Created elements to append within the created projectDiv, then appened the list of projecctDiv.

--- 100-projects-093-project-management-dashboard1.js
+++ 100-projects-093-project-management-dashboard1.js
@@ -20,7 +20,25 @@ const projects = [
 ];
 
 projects.forEach((project) => {
-    const projectDiv = document.createElement('div');
+    const projectDiv = document.createElement("div");
     projectDiv.classList.add("project");
-})
+
+    const projectName = document.createElement("h2");
+    projectName.textContent = project.name;
+    projectDiv.appendChild(projectName);
+
+    const projectDescription = document.createElement("p");
+    projectDescription.textContent = `Status: ${project.status}`;
+    projectDiv.appendChild(projectDescription);
+
+    const projectStatus = document.createElement("p");
+    projectStatus.textContent = `Status: ${projectStatus}`;
+    projectDiv.appendChild(projectStatus);
+
+    const projectTeam = document.createElement("p");
+    projectTeam.textContent = `Team: ${project.teamMembers.join(', ')}`;
+    projectDiv.appendChild(projectTeam);
+
+    projectList.appendChild(projectDiv);
+});
 
