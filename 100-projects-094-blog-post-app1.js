6002d4c70607c257074559f84b7ee53f936fece5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-24 07:30:10 -0600

    Made variables to store ids from html for use in JavaScript.

--- 100-projects-094-blog-post-app1.js
+++ 100-projects-094-blog-post-app1.js
@@ -1,2 +1,8 @@
 'use strict'
 
+const form = document.getElementById("post-form");
+const titleInput = document.getElementById("postTitle");
+const bodyInput = document.getElementById("postBody");
+const postList = document.getElementById("post-list");
+const API_URL = `http://my-json-server.typicode.com/SanghyuNa-web/blogmanage/posts`;
+
