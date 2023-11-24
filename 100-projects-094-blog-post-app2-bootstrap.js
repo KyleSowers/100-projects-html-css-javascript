ed75a22df65ddf19b522dec92f154e268f2e7fe2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-24 09:29:41 -0600

    Added pre-existing posts to display onto page initially (array of posts).

--- 100-projects-094-blog-post-app2-bootstrap.js
+++ 100-projects-094-blog-post-app2-bootstrap.js
@@ -5,3 +5,14 @@ const searchTitleInput = document.getElementById("searchTitle");
 const sortPostsInput = document.getElementById("sortPosts");
 const postList = document.getElementById('post-list');
 
+const posts = [
+    {
+        "title": "First Post",
+        "body": "This is the first post"
+    },
+    {
+        "title": "Second Post",
+        "body": "This is the second post"
+    }
+];
+
