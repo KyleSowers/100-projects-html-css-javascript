d72d173f91b707cf84bd418c6a20f4d667ad0546
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-24 08:18:17 -0600

    Added to existing posts and refactored the addPostToPage function to display premade posts.

--- 100-projects-094-blog-post-app1.js
+++ 100-projects-094-blog-post-app1.js
@@ -4,16 +4,49 @@ const form = document.getElementById("post-form");
 const titleInput = document.getElementById("postTitle");
 const bodyInput = document.getElementById("postBody");
 const postList = document.getElementById("post-list");
-const API_URL = `http://my-json-server.typicode.com/SanghyuNa-web/blogmanage/posts`;
 
-//Fetch initial posts
-fetch(API_URL)
-    .then((response) => response.json())
-    .then((posts) => {
-        posts.forEach((posts) => {
-            addPostToPage(post);
+
+const posts = [
+    {
+        "id": 1,
+        "title": "First Post",
+        "body": "This is the first post"
+    },
+    {
+        "id": 2,
+        "title": "Second Post",
+        "body": "This is the second post"
+    }
+];
+
+//Function to display posts
+function addPostToPage(posts) {
+    postList.innerHTML = "";
+
+    posts.forEach((post) => {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        const postTitle = document.createElement("h2");
+        postTitle.textContent = post.title;
+        postDiv.appendChild(postTitle);
+
+        const postBody = document.createElement("p");
+        postBody.textContent = postBody;
+        postDiv.appendChild(postBody);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener('click', () => {
+            //Real-world scenario, send delete request to API here
+            postList.removeChild(postDiv);
         });
-    });
+        postDiv.appendChild(deleteButton);
+        postList.appendChild(postDiv);
+    })
+}
+
+addPostToPage(posts);
 
 //Form Submission
 form.addEventListener("submit", (event) => {
@@ -31,26 +64,3 @@ form.addEventListener("submit", (event) => {
     titleInput.value = "";
     bodyInput.value = "";
 });
-
-function addPostToPage(post) {
-    const postDiv = document.createElement('div');
-    postDiv.classList.add('post');
-
-    const postTitle = document.createElement("h2");
-    postTitle.textContent = post.title;
-    postDiv.appendChild(postTitle);
-
-    const postBody = document.createElement("p");
-    postBody.textContent = postBody;
-    postDiv.appendChild(postBody);
-
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener('click', () => {
-        //Real-world scenario, send delete request to API here
-        postList.removeChild(postDiv);
-    });
-
-    postDiv.appendChild(deleteButton);
-    postList.appendChild(postDiv);
-}
