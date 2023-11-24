d511977f5e9e8fca88497eaf96c8d0df90d885fd
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-24 09:26:36 -0600

    Added variable of needed html ids.

--- 100-projects-094-blog-post-app2-bootstrap.js
+++ 100-projects-094-blog-post-app2-bootstrap.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const form = document.getElementById("post-form");
+const searchTitleInput = document.getElementById("searchTitle");
+const sortPostsInput = document.getElementById("sortPosts");
+const postList = document.getElementById('post-list');
+
