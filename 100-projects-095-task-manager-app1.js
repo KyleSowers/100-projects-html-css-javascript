c10e9f018ddf1e05713b754ac11518610a25d260
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-25 10:19:15 -0600

    Added constants that store html ids.

--- 100-projects-095-task-manager-app1.js
+++ 100-projects-095-task-manager-app1.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const form = document.getElementById('task-form');
+const titleInput = document.getElementById('taskTitle');
+const taskList = document.getElementById('task-list');
+const API_URL = `https://jsonplaceholder.typicode.com/todos`;
+
