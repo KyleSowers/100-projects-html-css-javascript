05545d099f91c9c68e1263248c8a127a8103307a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-25 15:35:03 -0600

    Added needed constant variables that pull from elements of the html (form/title/number/button/list/api).

--- 100-projects-095-task-manager-app2.js
+++ 100-projects-095-task-manager-app2.js
@@ -1,2 +1,8 @@
 'use strict'
 
+const form = document.getElementById('task-form');
+const titleInput = document.getElementById('taskTitle');
+const taskNumber = document.getElementById('taskNumber');
+const setTaskNumberButton = document.getElementById('setTaskNumber');
+const taskList = document.getElementById('task-list');
+const API_URL = `https://jsonplaceholder.typicode.com/todos`;
