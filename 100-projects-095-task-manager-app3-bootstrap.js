ed19c38164eba558f943862335075451673183c5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-25 16:47:21 -0600

    Added constant variables pulling required elements from html to include a taskStatusFilter not included in previous version of the task-manager-app projects. It allows for selection between all tasks, completed tasks or incomplete tasks.

--- 100-projects-095-task-manager-app3-bootstrap.js
+++ 100-projects-095-task-manager-app3-bootstrap.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const form = document.getElementById('task-form');
+const titleInput = document.getElementById('taskTitle');
+const taskNumberInput = document.getElementById('taskNumber');
+const setTaskNumberButton = document.getElementById('setTaskNumber');
+const taskStatusfilter = document.getElementById('task-status-filter');
+const taskList = document.getElementById('task-list');
+const API_URL = `https://jsonplaceholder.typicode.com/todos`;
