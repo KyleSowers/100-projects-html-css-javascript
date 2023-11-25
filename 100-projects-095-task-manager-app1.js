5432ab510b7639b3697e9d2137c083412111a6f3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-25 10:23:15 -0600

    Added the fetch request for initial posts.

--- 100-projects-095-task-manager-app1.js
+++ 100-projects-095-task-manager-app1.js
@@ -5,3 +5,13 @@ const titleInput = document.getElementById('taskTitle');
 const taskList = document.getElementById('task-list');
 const API_URL = `https://jsonplaceholder.typicode.com/todos`;
 
+//fetch initial tasks
+fetch(API_URL)
+    .then((response) => response.json())
+    .then((tasks) => {
+        tasks.forEach((task) => {
+            addTaskToPage(task);
+        });
+    });
+//Real-world you would send POST request to API here
+addTaskToPage(task);
