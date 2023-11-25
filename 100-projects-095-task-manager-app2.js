e618e5f5569a49d8117823b647e172a7a33b3a0c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-25 15:42:03 -0600

    Added function to fetch initial tasks through api, the event listener for form submit and the constructor for a task.

--- 100-projects-095-task-manager-app2.js
+++ 100-projects-095-task-manager-app2.js
@@ -2,7 +2,33 @@
 
 const form = document.getElementById('task-form');
 const titleInput = document.getElementById('taskTitle');
-const taskNumber = document.getElementById('taskNumber');
+const taskNumberInput = document.getElementById('taskNumber');
 const setTaskNumberButton = document.getElementById('setTaskNumber');
 const taskList = document.getElementById('task-list');
 const API_URL = `https://jsonplaceholder.typicode.com/todos`;
+
+//Fetch initial Tasks
+function fetchTasks() {
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((tasks) => {
+            taskList.innerHTML = '';
+            tasks.slice(0, taskNumberInput.value).forEach((task) => {
+                addTaskToPage(tasks);
+            });
+        });
+}
+
+fetchTasks();
+
+//Set a number of tasks
+setTaskNumberButton.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const task = {
+        title: titleInput.value;
+        completed: false;
+    };
+
+
+})
