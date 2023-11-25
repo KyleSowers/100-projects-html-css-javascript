489f970bbde56c777d291bd77bf942d3f9ef68e7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-25 17:03:19 -0600

    Added event listeners for both buttons. The form button submit creates the task for the list and the number button fetches the number requested.

--- 100-projects-095-task-manager-app3-bootstrap.js
+++ 100-projects-095-task-manager-app3-bootstrap.js
@@ -7,3 +7,39 @@ const setTaskNumberButton = document.getElementById('setTaskNumber');
 const taskStatusfilter = document.getElementById('task-status-filter');
 const taskList = document.getElementById('task-list');
 const API_URL = `https://jsonplaceholder.typicode.com/todos`;
+
+function fetchTasks() {
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((tasks) => {
+            taskList.innerHTML = '';
+            tasks.slice(0, taskNumberInput.value).forEach((task) => {
+                addTaskToPage(task);
+            });
+        });
+}
+
+fetchTasks();
+
+//Set Number of tasks
+setTaskNumberButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    fetchTasks();
+});
+
+//Form submission
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const task = {
+        title: titleInput.value,
+        completed: false
+    };
+
+    //Add task at the top of the list
+    addTaskToPage(task, true);
+
+    //clear the input field
+    titleinput.value = '';
+});
+
