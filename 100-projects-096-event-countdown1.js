679d858fdc845f9a134c39d03d693ede4737cc0d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-26 06:35:57 -0600

    Createed constants storing values of html elements.

--- 100-projects-096-event-countdown1.js
+++ 100-projects-096-event-countdown1.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const form = document.getElementById('event-form');
+const nameInput = document.getElementById('eventName');
+const timeInput = document.getElementById('eventTime');
+const countdownTitle = document.getElementById('countdown-title');
+const countdownEl = document.getElementById('countdown');
