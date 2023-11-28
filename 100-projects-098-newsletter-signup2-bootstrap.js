73c840259b993c56e62947558ea4f4780f52b693
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-28 17:26:49 -0600

    Began form submittal event listerner with prevent default, then addition of variables, including a email pattern regex.

--- 100-projects-098-newsletter-signup2-bootstrap.js
+++ 100-projects-098-newsletter-signup2-bootstrap.js
@@ -1,2 +1,12 @@
 "use strict"
 
+$('#signup-form').on('submit', function(event) => {
+    event.preventDefault();
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var interest = $('#interests').val();
+    var emailPattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+
+})
