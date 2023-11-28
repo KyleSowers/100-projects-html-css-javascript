f191f64420044fa8e951bb671f187594cc4b2110
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-27 19:57:04 -0600

    Added a query all button selector that runs by event listener and provides action based on the button type method.

--- 100-projects-097-web-calculator1.js
+++ 100-projects-097-web-calculator1.js
@@ -2,3 +2,30 @@
 
 const display = document.querySelector("#display");
 
+document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.hasAttribute("data-number") || button.hasAttribute("data-operator") || button.hasAttribute("data-decimal")) {
+            display.value += button.textContent;
+        } else if (button.hasAttribute("data-equal")) {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Error";
+            }
+        } else if (button.hasAttribute("data-clear")) {
+            display.value = "";
+        } else if (button.hasAttribute("data-sign")) {
+            if (display.value) {
+                display.value = parseFloat(display.value) / 100;
+            }
+        } else if (button.hasAttribute("data-sqrt")) {
+            if (display.value) {
+                display.value = Math.sqrt(parseFloat(display.value)).toFixed(2);
+            }
+        } else if (button.hasAttribute("data-square")) {
+            if (display.value) {
+                display.value = Math.pow(parseFloat(display.value), 2);
+            }
+        }
+    });
+});
