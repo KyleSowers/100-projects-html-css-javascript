e1d9c90a3c725b68e339c6c98df57d575e1fc4b9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-28 17:38:18 -0600

    Added HTML element with bootstrap class styling. Then created the same functionality for foem as 100-projects-098-newsletter-signup1 project, but with jquery. Added a password input that also has a checkbox selection to make password input visibile.

--- 100-projects-098-newsletter-signup2-bootstrap.js
+++ 100-projects-098-newsletter-signup2-bootstrap.js
@@ -1,6 +1,6 @@
 "use strict"
 
-$('#signup-form').on('submit', function(event) => {
+$('#signup-form').on('submit', function (event) {
     event.preventDefault();
 
     var name = $('#name').val();
@@ -8,5 +8,25 @@ $('#signup-form').on('submit', function(event) => {
     var interest = $('#interests').val();
     var emailPattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address");
+        return false;
+    }
 
-})
+    alert("Thank you for signing up!");
+
+    $('#name').val('');
+    $('#email').val('');
+    $('#interests').val('');
+    $('#password').val('');
+});
+
+$("#show-password").on('click', function () {
+    var passwordField = $('#password');
+    var passwordFieldType = passwordField.attr("type");
+    if (passwordFieldType == 'password') {
+        passwordField.attr('type', 'text');
+    } else {
+        passwordField.attr('type', 'password');
+    }
+});
