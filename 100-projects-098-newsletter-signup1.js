002d34c20d320e50b39013bf4f617c3f7efec247
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-28 16:48:05 -0600

    Started function with aprevent default and then call for variable values from user inputs.'

--- 100-projects-098-newsletter-signup1.js
+++ 100-projects-098-newsletter-signup1.js
@@ -1,2 +1,14 @@
 'use strict'
 
+function submitForm(event) {
+    event.preventDefault(); //Prevent form submittal
+
+    //Get form values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var interests = document.getElementById('interests').value;
+
+    //
+
+
+}
