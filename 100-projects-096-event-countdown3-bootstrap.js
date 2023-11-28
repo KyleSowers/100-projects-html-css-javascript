c2629d6389ca4cbb39bd7908bae196f08e731936
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-27 18:51:38 -0600

    Created function for event form submit that takes in user values, but will alert if a field is missing or time is in the past.

--- 100-projects-096-event-countdown3-bootstrap.js
+++ 100-projects-096-event-countdown3-bootstrap.js
@@ -1,2 +1,20 @@
 'use strict'
 
+$('#event-form').on('submit', function (event) {
+    event.preventDefault();
+
+    let eventName = $('#eventName').val();
+    let eventTime = new Date($('#eventTime').val());
+
+    if (!eventName || !eventTime) {
+        alert("Both fields are required!");
+        return;
+    }
+
+    if (eventTime < new Date()) {
+        alert("Event time cannot be in the past!");
+        return;
+    }
+
+
+})
