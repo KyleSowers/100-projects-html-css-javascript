6cc74e1c6d857bc64b9e5291d37dba18b4bc55af
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-29 07:41:04 -0600

    Set auto rotate function for 3 seconds interval on slides.

--- 100-projects-099-image-carousel1.js
+++ 100-projects-099-image-carousel1.js
@@ -5,3 +5,7 @@ let carouselImages = Array.from(document.getElementsByClassName('carousel-image'
 let numSlides = carouselImages.length;
 let intervalid;
 
+//Auto rotate images every 3 seconds
+intervalid = setInterval(() => {
+    changeSlide(1);
+}, 3000);
