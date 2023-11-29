e65bce13c5c057a1fb9921e6eea3da712d916421
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-29 08:58:51 -0600

    Added javascript ready carousel function, to show a dot indicator for number of images and current image.

--- 100-projects-099-image-carousel2-bootstrap.js
+++ 100-projects-099-image-carousel2-bootstrap.js
@@ -1,2 +1,16 @@
 'use strict'
 
+$(document).ready(function () {
+    $('#carouselExampleIndicators').carousel({
+        interval: 3000,
+        pause: false,
+    });
+
+    $('.carousel-control-prev, .carousel-control-next').click(function () {
+        $('#carouselExampleIndicators').carousel('pause');
+        setTimeout(function () {
+            $('#carouselExampleIndicators').carousel('cycle');
+        }, 5000);
+    });
+});
+
