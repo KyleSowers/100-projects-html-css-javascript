5a5243673d03f0f3c508ef88c6e2ac23cd62de9a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-30 10:56:35 -0600

    Added startTest function that disables startBtn and puts focus on text while setting time interval.

--- 100-projects-100-typing-speed-tester1.js
+++ 100-projects-100-typing-speed-tester1.js
@@ -4,6 +4,14 @@ let testText = document.getElementById('test-text');
 let result = document.getElementById('result');
 let timer = document.getElementById('timer');
 let startBtn = document.getElementById('start-btn');
-let intervalid;
+let intervalId;
 let timeRemaining = 60;
+
+function startTest() {
+    startBtn.disabled = true;
+    testText.disabled = false;
+    testText.focus();
+    timer.textContent = timeRemaining;
+    intervalId = setInterval(countdown, 1000);
+}
 
