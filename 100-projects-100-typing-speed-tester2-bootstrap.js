ae83eaf2625168fa160c433023f5f07a925e3e14
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-30 11:49:10 -0600

    Called upon html for values stored in variables.

--- 100-projects-100-typing-speed-tester2-bootstrap.js
+++ 100-projects-100-typing-speed-tester2-bootstrap.js
@@ -1,2 +1,12 @@
 'use strict'
 
+$(document).ready(function () {
+    let testText = $('#test-text');
+    let result = $('#result');
+    let progressBar = $('#progress-bar');
+    let startBtn = $('#start-btn');
+    let intervalId;
+    let timeRemaining = 60;
+    let errorCount = 0;
+
+})
