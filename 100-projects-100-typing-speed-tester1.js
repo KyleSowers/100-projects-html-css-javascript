f78fcb7dbc0484dff35295ac57a8db7a86575a08
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-30 10:50:57 -0600

    Added constants tht pull values needed from html setup.

--- 100-projects-100-typing-speed-tester1.js
+++ 100-projects-100-typing-speed-tester1.js
@@ -1,2 +1,9 @@
 'use strict'
 
+let testText = document.getElementById('test-text');
+let result = document.getElementById('result');
+let timer = document.getElementById('timer');
+let startBtn = document.getElementById('start-btn');
+let intervalid;
+let timeRemaining = 60;
+
