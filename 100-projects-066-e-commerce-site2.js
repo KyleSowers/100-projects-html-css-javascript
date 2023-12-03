51c283e7332674ae0e46f4cc9059c660fbdfc770
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-02 20:07:24 -0600

    Adjustd body element display. Added images to version two and three of project-066 ecommerce.

--- 100-projects-066-e-commerce-site2.js
+++ 100-projects-066-e-commerce-site2.js
@@ -1,12 +1,12 @@
 'use strict'
 
 let products = [
-    { id: 1, name: 'Shoes 1', price: 100, category: 'Shoes', image: 'e-shoes1.png', description: 'Comfortable running shoes.' },
-    { id: 2, name: 'Hat 1', price: 50, category: 'Hats', image: 'e-hat1.png', description: 'Stylish baseball cap.' },
-    { id: 3, name: 'T-shirt 1', price: 20, category: 'Clothes', image: 'e-tshirt1.png', description: 'Cotton t-shirt' },
-    { id: 4, name: 'Shoes 2', price: 100, category: 'Shoes', image: 'e-shoes2.jpg', description: 'Comfortable running shoes.' },
-    { id: 5, name: 'Hat 2', price: 50, category: 'Hats', image: 'e-hat2.jpg', description: "Stylish baseball cap." },
-    { id: 6, name: 'T-shirt 2', price: 20, category: 'Clothes', image: 'e-tshirt2.jpg', description: 'Cotton t-shirt' }
+    { id: 1, name: 'Shoes 1', price: 100, category: 'Shoes', image: 'images/e-shoes1.jpeg', description: 'Comfortable running shoes.' },
+    { id: 2, name: 'Hat 1', price: 50, category: 'Hats', image: 'images/e-hat1.jpeg', description: 'Stylish baseball cap.' },
+    { id: 3, name: 'T-shirt 1', price: 20, category: 'Clothes', image: 'images/e-tshirt1.jpeg', description: 'Cotton t-shirt' },
+    { id: 4, name: 'Shoes 2', price: 100, category: 'Shoes', image: 'images/e-shoes2.jpeg', description: 'Comfortable running shoes.' },
+    { id: 5, name: 'Hat 2', price: 50, category: 'Hats', image: 'images/e-hat2.jpeg', description: "Stylish baseball cap." },
+    { id: 6, name: 'T-shirt 2', price: 20, category: 'Clothes', image: 'images/e-tshirt2.jpeg', description: 'Cotton t-shirt' }
 ];
 
 let cart = [];
