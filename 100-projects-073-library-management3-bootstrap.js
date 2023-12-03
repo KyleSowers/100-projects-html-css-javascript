d61d81b04b77d74e42b09df55b6d47954ef86ab1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-02 22:13:10 -0600

    Added additional div to 100-projects carousel. Created video of project-073 and added to new div.

--- 100-projects-073-library-management3-bootstrap.js
+++ 100-projects-073-library-management3-bootstrap.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         var status = $('#status').val();
 
         var bookDiv = $('<div class="card mb-4 p-3">').html(`
-        <h5 class="card-text">Pages: ${title}</h5>
+        <h5 class="card-text">Title: ${title}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${author}</h6>
         <p class="card-text">Pages: ${pages}</p>
         <p class="card-text">Status: ${status}</p>
